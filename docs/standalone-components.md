604f627df9e793a3303f4f06abece20c6abe4a44
kevin <contact@kevinchevallier.com>
2017-12-21 18:40:01 -0200

    Introduce sections

---
+++ docs/standalone-components.md
@@ -0,0 +1,5 @@
+* **React-csv-parse**
+  https://github.com/vtex/react-csv-parse
+
+* **Intl-container**
+  https://github.com/vtex/intl-container
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,32 @@
 module.exports = {
-  require: [
-    'vtex-tachyons'
+  require: ['vtex-tachyons'],
+  showUsage: true,
+  sections: [
+    {
+      name: 'Introduction',
+      content: 'docs/introduction.md',
+    },
+    {
+      name: 'Documentation',
+      sections: [
+        {
+          name: 'Installation',
+          content: 'docs/one.md',
+          description: 'The description for the one section',
+        },
+        {
+          name: 'Configuration',
+          content: 'docs/two.md',
+        },
+      ],
+    },
+    {
+      name: 'Bundled components',
+      components: 'src/components/**/*.js',
+    },
+    {
+      name: 'Standalone components',
+      content: 'docs/standalone-components.md',
+    },
   ],
-  showUsage: true
 }
