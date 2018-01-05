881d3f47a000ad8f318d2c5ded81a214acb64821
Breno Calazans <brenocalazans@gmail.com>
2018-01-05 17:47:29 -0200

    Implement Button component

--- src/components/Button/README.md
+++ src/components/Button/README.md
@@ -1,5 +1,31 @@
 Button
 
+Default
 ```js
-<Button>Save</Button>
+<Button>Cancel</Button>
+```
+
+Secondary
+```js
+<Button secondary>Save</Button>
+```
+
+Primary
+```js
+<Button primary>Save</Button>
+```
+
+Disabled
+```js
+<Button disabled>Save</Button>
+```
+
+Side by side
+```js
+<div>
+  <Button>Cancel</Button>
+  <Button secondary>Save</Button>
+  <Button primary>Save</Button>
+  <Button disabled>Save</Button>
+</div>
 ```
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,16 +3,52 @@ import PropTypes from 'prop-types'
 
 class Button extends Component {
   render() {
+    const { primary, secondary, disabled } = this.props
+
+    if (secondary && primary) {
+      throw new Error('Button component cannot be primary AND secondary')
+    }
+
+    let classes = 'ph4 pv2 ttu br2 fw4 f7 '
+
+    if (!secondary && !primary && !disabled) {
+      classes += 'bw0 blue hover-bg-light-gray bg-animate '
+    }
+
+    if (secondary && !disabled) {
+      classes += 'bw1 ba b--blue blue hover-bg-blue hover-white bg-animate '
+    }
+
+    if (primary && !disabled) {
+      classes += 'bw1 ba b--blue bg-blue white hover-bg-dark-blue hover-b-dark-blue bg-animate '
+    }
+
+    if (disabled) {
+      classes += 'bw0 bg-moon-gray gray '
+    } else {
+      classes += 'pointer '
+    }
+
     return (
-      <button className={`bg-black ${this.props.primary ? 'red' : 'silver'}`}>
-        XAU {this.props.children}
+      <button className={`${classes}`}>
+        {this.props.children}
       </button>
     )
   }
 }
 
+Button.defaultProps = {
+  primary: false,
+  secondary: false,
+  disabled: false,
+  htmlProps: {},
+}
+
 Button.propTypes = {
   primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  disabled: PropTypes.bool,
+  htmlProps: PropTypes.object,
   children: PropTypes.node.isRequired,
 }
 
