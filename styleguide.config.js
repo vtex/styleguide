2d0151c76990d439e8af6a52ce917a4802584d4d
Breno Calazans <brenocalazans@gmail.com>
2018-01-18 17:10:09 -0200

    Remove sections

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,34 +3,6 @@ const path = require('path')
 module.exports = {
   require: ['vtex-tachyons'],
   showUsage: true,
-  sections: [
-    {
-      name: 'Introduction',
-      content: 'docs/introduction.md',
-    },
-    {
-      name: 'Documentation',
-      sections: [
-        {
-          name: 'Installation',
-          content: 'docs/one.md',
-          description: 'The description for the one section',
-        },
-        {
-          name: 'Configuration',
-          content: 'docs/two.md',
-        },
-      ],
-    },
-    {
-      name: 'Bundled components',
-      components: 'src/components/**/*.js',
-    },
-    {
-      name: 'Standalone components',
-      content: 'docs/standalone-components.md',
-    },
-  ],
   getComponentPathLine(componentPath) {
     const pathArray = path.dirname(componentPath).split(path.sep)
     const componentName = pathArray[pathArray.length - 1]
