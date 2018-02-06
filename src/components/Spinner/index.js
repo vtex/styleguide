db0f685140a4e90ee849f6e512f8e435c0c432dc
kevin <contact@kevinchevallier.com>
2018-02-06 18:07:30 -0200

    Update loader to spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Loader = () => (
+const Spinner = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 100 100"
@@ -30,4 +30,4 @@ const Loader = () => (
   </svg>
 )
 
-export default Loader
+export default Spinner
