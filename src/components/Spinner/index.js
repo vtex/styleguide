f27bc7b26863831b4054fdea119bdc7e0ac58405
kevin <contact@kevinchevallier.com>
2018-02-21 15:10:06 -0300

    Add spinner within button

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './index.css'
 
-const Spinner = ({ secondary }) => (
+const Spinner = ({ secondary, width, heigth }) => (
   <svg
+    width={width}
+    heigth={heigth}
     className="spinner"
     viewBox="0 0 66 66"
     xmlns="http://www.w3.org/2000/svg"
@@ -23,10 +25,14 @@ const Spinner = ({ secondary }) => (
 
 Spinner.propTypes = {
   secondary: PropTypes.bool,
+  width: PropTypes.number,
+  heigth: PropTypes.number,
 }
 
 Spinner.defaultProps = {
   secondary: false,
+  width: '',
+  heigth: '',
 }
 
 export default Spinner
