d37088fe3e969d1e86a481bc4e0e96c3577579b3
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2018-03-08 16:46:38 -0300

    clean up code

--- src/components/Dropdown/README.md
+++ src/components/Dropdown/README.md
@@ -2,17 +2,64 @@ Dropdown
 
 Active
 ```js
-<Dropdown 
-    placeholder="Placeholder" 
-    options={["op1", "op2", "op3", "op4"]}>
-</Dropdown>
+class Example extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        open: false,
+        value: undefined,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e, value) {
+    this.setState({ value })
+  }
+    
+  render() {
+    return (
+      <Dropdown 
+        placeholder='Placeholder'
+        options={['Value 1', 'Value 2', 'Value 3', 'Value 4']}
+        onChange={this.handleChange}
+        value={this.state.value}
+        {...this.props}
+      />
+    )
+  }
+};
+<Example />
 ```
 
 Disabled
 ```js
-<Dropdown 
-    placeholder="Placeholder" 
-    options={["op1", "op2", "op3", "op4"]}
-    disabled={true}>
-</Dropdown>
+class Example extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false,
+      value: undefined,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e, value) {
+    this.setState({ value })
+  }
+    
+  render() {
+    return (
+      <Dropdown 
+        placeholder='Placeholder'
+        options={['Value 1', 'Value 2', 'Value 3', 'Value 4']}
+        onChange={this.handleChange}
+        value={this.state.value}
+        {...this.props}
+      />
+    )
+  }
+};
+<Example disabled />
 ```
--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -5,8 +5,7 @@ class Dropdown extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      menuOpen: false,
-      selectedValue: undefined,
+      open: false,
     }
   }
   componentDidMount() {
@@ -21,71 +20,45 @@ class Dropdown extends Component {
     this.wrapperRef = node;
   }
 
-  handleClickOutside = (event) => {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.setState({menuOpen: false})
+  handleClickOutside = (e) => {
+    if (this.wrapperRef && !this.wrapperRef.contains(e.target)) {
+      this.props.onClose && this.props.onClose(e)
     }
   }
 
-  handleClick = () => {
+  handleClick = (e) => {
+    const { onOpen, onClose } = this.props
     document.getElementById('select').blur()
-    this.setState({menuOpen: !this.state.menuOpen})
+    this.setState({open: !this.state.open}, () => {
+      this.state.open ? onOpen && onOpen(e) : onClose && onClose(e)
+    })
   }
 
-  handleOptionClick = (option) => {
-    !this.props.disabled && this.props.onChange && this.props.onChange(option)
-    this.setState({menuOpen: false})
-    this.setState({selectedValue: option})
-  }
+  handleOptionClick = (e, option) => {
+    const { disabled, onChange, onClose } = this.props
 
-  renderArrow = () => {
-    const color = this.props.disabled ? '#969799' : '#368DF7'
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-        <g class="nc-icon-wrapper" fill={color}>
-            <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path>
-        </g>
-    </svg>
-    )
-  }
-
-  renderMenu = () => {
-    const { options } = this.props
-
-    return (
-      options.map(option => {
-        return (
-          <button 
-            className='flex w-100 right pa4 hover-bg-near-white near-black'
-            onClick={() => this.handleOptionClick(option)}
-            style={{height: '44px', 'border': '0px', 'borderBottom': '1px solid #F5F7FA'}}>
-            {option}
-          </button>
-        )
-      })
-    )
+    !disabled && onChange && onChange(e, option)
+    this.setState({open: false}, () => {
+      onClose && onClose(e)
+    })
   }
 
   getButtonClass = () => {
-    const { disabled } = this.props
-    let className
-    if (disabled) {
-      className = 'pa4 bn br2 bg-light-gray gray'
-    } else {
-      const textColor = this.state.selectedValue ? 'near-black' : 'gray'
-      className = `pa4 ba bw1 br2 ${textColor} bg-white b--near-white hover-bg-near-white hover-blue`
+    if (this.props.disabled) {
+      return 'pa4 bn br2 bg-light-gray gray'
     }
-    return className
+    const textColor = this.state.selectedValue ? 'near-black' : 'gray'
+    return `pa4 ba bw1 br2 ${textColor} bg-white b--near-white hover-bg-near-white hover-blue`
   }
 
   render() {
-    const { placeholder, disabled } = this.props
-    const { selectedValue, menuOpen } = this.state
-    const value = selectedValue ? selectedValue : placeholder
+    const { value, placeholder, disabled, options } = this.props
+    const { open } = this.state
     const optionsStyle = {'overflowY': 'scroll', 'height': '120px'}
-    if (menuOpen) {
+    if (open) {
       optionsStyle['boxShadow'] = '0px 0px 4px 0px rgba( 0, 0, 0, .15)'
     }
+
     return (
       <div ref={this.setWrapperRef}>
           <button
@@ -95,16 +68,31 @@ class Dropdown extends Component {
             onClick={this.handleClick}
             style={{outline: 'none', height: '44px'}}>
             <div className='flex justify-between'>
-              { value }
-              <div className='flex items-center pl6'> { this.renderArrow() } </div>
+              { value || placeholder }
+              <div className='flex items-center pl6'> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+                  <g class="nc-icon-wrapper" fill={disabled ? '#969799' : '#368DF7'}>
+                    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path>
+                  </g>
+                </svg>
+              </div>
             </div>
           </button>
-          { menuOpen && 
+          { open && 
             (
               <div 
                 className={'w-100 mw6 ba br2 b--near-white bg-white flex-column'}
                 style={optionsStyle}> 
-                  { this.renderMenu() }
+                { 
+                  options.map(option => (
+                    <button 
+                      className='flex w-100 right pa4 hover-bg-near-white near-black'
+                      onClick={(e) => this.handleOptionClick(e, option)}
+                      style={{height: '44px', 'border': '0px', 'borderBottom': '1px solid #F5F7FA'}}>
+                      {option}
+                    </button>
+                  ))
+                }
               </div>
             )
           }
@@ -114,8 +102,11 @@ class Dropdown extends Component {
 }
 
 Dropdown.propTypes = {
-  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
   onChange: PropTypes.func,
+  onOpen: PropTypes.func,
+  onClose: PropTypes.func,
   options: PropTypes.array.isRequired,
   disabled: PropTypes.bool,
 }
