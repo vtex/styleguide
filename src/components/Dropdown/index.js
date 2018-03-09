8ecd64a60f8e9bd581bab3e09741383d575ae214
Breno Calazans <brenocalazans@gmail.com>
2018-03-09 02:16:03 -0300

    Fix Dropdown CSS

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -9,39 +9,39 @@ class Dropdown extends Component {
     }
   }
   componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
+    document.addEventListener('mousedown', this.handleClickOutside)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
-  setWrapperRef = (node) => {
-    this.wrapperRef = node;
-  }
+  setWrapperRef = node => {
+    this.wrapperRef = node
+  };
 
-  handleClickOutside = (e) => {
+  handleClickOutside = e => {
     if (this.wrapperRef && !this.wrapperRef.contains(e.target)) {
       this.props.onClose && this.props.onClose(e)
     }
-  }
+  };
 
-  handleClick = (e) => {
+  handleClick = e => {
     const { onOpen, onClose } = this.props
-    document.getElementById('select').blur()
-    this.setState({open: !this.state.open}, () => {
+    this.select && this.select.blur()
+    this.setState({ open: !this.state.open }, () => {
       this.state.open ? onOpen && onOpen(e) : onClose && onClose(e)
     })
-  }
+  };
 
   handleOptionClick = (e, option) => {
     const { disabled, onChange, onClose } = this.props
 
     !disabled && onChange && onChange(e, option)
-    this.setState({open: false}, () => {
+    this.setState({ open: false }, () => {
       onClose && onClose(e)
     })
-  }
+  };
 
   getButtonClass = () => {
     if (this.props.disabled) {
@@ -49,53 +49,61 @@ class Dropdown extends Component {
     }
     const textColor = this.state.selectedValue ? 'near-black' : 'gray'
     return `pa4 ba bw1 br2 ${textColor} bg-white b--near-white hover-bg-near-white hover-blue`
-  }
+  };
 
   render() {
     const { value, placeholder, disabled, options } = this.props
     const { open } = this.state
-    const optionsStyle = {'overflowY': 'scroll', 'height': '120px'}
+    const optionsStyle = { overflowY: 'scroll', height: '120px' }
     if (open) {
       optionsStyle['boxShadow'] = '0px 0px 4px 0px rgba( 0, 0, 0, .15)'
     }
 
     return (
-      <div ref={this.setWrapperRef}>
-          <button
-            disabled={disabled}
-            id={'select'}
-            className={this.getButtonClass()}
-            onClick={this.handleClick}
-            style={{outline: 'none', height: '44px'}}>
-            <div className='flex justify-between'>
-              { value || placeholder }
-              <div className='flex items-center pl6'> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-                  <g class="nc-icon-wrapper" fill={disabled ? '#969799' : '#368DF7'}>
-                    <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path>
-                  </g>
-                </svg>
-              </div>
+      <div className="relative" ref={this.setWrapperRef}>
+        <button
+          disabled={disabled}
+          ref={(el) => (this.select = el)}
+          className={this.getButtonClass()}
+          onClick={this.handleClick}
+          style={{ outline: 'none', height: '44px' }}
+        >
+          <div className="flex justify-between">
+            {value || placeholder}
+            <div className="flex items-center pl6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+              >
+                <g fill={disabled ? '#969799' : '#368DF7'}>
+                  <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
+                </g>
+              </svg>
             </div>
-          </button>
-          { open && 
-            (
-              <div 
-                className={'w-100 mw6 ba br2 b--near-white bg-white flex-column'}
-                style={optionsStyle}> 
-                { 
-                  options.map(option => (
-                    <button 
-                      className='flex w-100 right pa4 hover-bg-near-white near-black'
-                      onClick={(e) => this.handleOptionClick(e, option)}
-                      style={{height: '44px', 'border': '0px', 'borderBottom': '1px solid #F5F7FA'}}>
-                      {option}
-                    </button>
-                  ))
-                }
-              </div>
-            )
-          }
+          </div>
+        </button>
+        {open &&
+          <div
+            className="absolute w-100 mw6 ba br2 b--near-white bg-white flex-column"
+            style={optionsStyle}
+          >
+            {options.map(option => (
+              <button
+                key={option}
+                className="flex w-100 right pa4 hover-bg-near-white near-black"
+                onClick={e => this.handleOptionClick(e, option)}
+                style={{
+                  height: '44px',
+                  border: '0px',
+                  borderBottom: '1px solid #F5F7FA',
+                }}
+              >
+                {option}
+              </button>
+            ))}
+          </div>}
       </div>
     )
   }
