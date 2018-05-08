3505425db27d4c017e813618358683659d2e5a46
kevinch <kevin.chevallier@vtex.com.br>
2018-05-08 15:20:42 -0300

    Add upload icon

--- src/components/icon/Upload/index.js
+++ src/components/icon/Upload/index.js
@@ -2,20 +2,20 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import config from 'vtex-tachyons/config.json'
 
-class Download extends PureComponent {
+class Upload extends PureComponent {
   render() {
     const { color, size } = this.props
     return (
       <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}>
         <g
-          className="vtex-icon__download"
+          className="vtex-icon__upload"
           fill="none"
           stroke={color}
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeMiterlimit="10"
         >
-          <path d="M8.5.5v11M13.5 6.5l-5 5-5-5" />
+          <path d="M8.5 11.5V.5M13.5 5.5l-5-5-5 5" />
           <path data-color="color-2" d="M15.5 15.5h-14" />
         </g>
       </svg>
@@ -23,14 +23,14 @@ class Download extends PureComponent {
   }
 }
 
-Download.defaultProps = {
+Upload.defaultProps = {
   color: config.colors['serious-black'],
   size: 16,
 }
 
-Download.propTypes = {
+Upload.propTypes = {
   color: PropTypes.string,
   size: PropTypes.number,
 }
 
-export default Download
+export default Upload
