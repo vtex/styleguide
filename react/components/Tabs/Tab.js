dab4f12a9cb8f313cb5bea3e47b2715bf645467c
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-06-19 17:34:33 -0300

    Add Tab component

---
+++ react/components/Tabs/Tab.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Tab extends Component {
+  handleClick = e => {
+    this.props.onClick && this.props.onClick(e)
+  }
+
+  render() {
+    const { active, label } = this.props
+    return (
+      <button
+        type="button"
+        onClick={this.handleClick}
+        className={`vtex-tab__button pointer bt-0 bl-0 br-0 bw1 ${
+          active
+            ? 'near-black b--rebel-pink'
+            : 'mid-gray b--transparent'}
+          hover-near-black fw5 fw4 v-mid relative pv5 ph4 f5 bg-transparent outline-0`}
+      >
+        {label}
+      </button>
+    )
+  }
+}
+
+Tab.propTypes = {
+  active: PropTypes.bool,
+  children: PropTypes.node,
+  label: PropTypes.any.isRequired,
+  onClick: PropTypes.func,
+}
