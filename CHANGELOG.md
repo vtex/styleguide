71e7cd7485ae54d087dae54f9bffb0f138d99ced
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-06-21 13:41:49 -0300

    Wrap alert icon in a div

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- **Alert** wrap icon in a div to always maintain its size
+
 ## [4.3.4] - 2018-6-20
 
 ### Added
--- react/components/Alert/index.js
+++ react/components/Alert/index.js
@@ -55,7 +55,7 @@ class Alert extends Component {
     return (
       <div className={`vtex-alert flex justify-between f5 near-black ${classes}`}>
         <div className="flex items-center">
-          {showIcon && <Icon color={color} size={18} />}
+          {showIcon && <div><Icon color={color} size={18} /></div>}
 
           <div className={`${showIcon ? 'ph5 flex' : 'pr5'}`}>
             {this.props.children}
