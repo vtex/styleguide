38d398cf93de26e2135c239ef84035d202a50194
Lucas Bebber <lucasbbebber@gmail.com>
2018-07-17 16:13:41 -0300

    Disable version script temporarily

--- scripts/version.js
+++ scripts/version.js
@@ -7,7 +7,14 @@ const manifestJsonPath = path.join(__dirname, '../manifest.json')
 const pkg = JSON.parse(fs.readFileSync(packageJsonPath).toString())
 const manifest = JSON.parse(fs.readFileSync(manifestJsonPath).toString())
 
-console.log(`Updating manifest version from ${manifest.version} to ${pkg.version}`)
-manifest.version = pkg.version
+// TODO: Fix this
+// (this is run pre-releasy, so the new version is not updated on package.json
+// by the time this script is run. The result is that it tries to update to the
+// same version, and fail (e.g. Updating version from 5.2.3 to 5.2.3)
 
-fs.writeFileSync(manifestJsonPath, JSON.stringify(manifest, null, 2))
+// console.log(`Updating manifest version from ${manifest.version} to ${pkg.version}`)
+// manifest.version = pkg.version
+
+// fs.writeFileSync(manifestJsonPath, JSON.stringify(manifest, null, 2))
+
+console.log(`Updating manifest version from ${pkg.version} to ${manifest.version}`)
