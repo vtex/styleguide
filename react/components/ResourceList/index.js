f92fd2915954bbf13153d09cd5ab3b0be8c6fe73
kevinch <kevin.chevallier@vtex.com.br>
2018-08-20 13:17:07 -0300

    Implement pagination

--- react/components/ResourceList/index.js
+++ react/components/ResourceList/index.js
@@ -8,46 +8,69 @@ import Button from '../Button'
 import IconDownload from '../icon/Download'
 
 class ResourceList extends PureComponent {
+  handlePaginationNext = () => {
+    this.props.pagination.onNextClick && this.props.pagination.onNextClick()
+  }
+  handlePaginationPrev = () => {
+    this.props.pagination.onPrevClick && this.props.pagination.onPrevClick()
+  }
+
   render() {
+    const { table, exportBtn, pagination } = this.props
+    const showExportBtn = exportBtn && exportBtn.show
+
     return (
       <div className="vtex-resourceList__container">
         <div className="mb5 flex flex-row">
           <Input placeholder="Search..." />
-          <Button variation="primary" size="small">
-            <span className="flex align-baseline">
-              <span className="mr3">
-                <IconDownload color="currentColor" />
+
+          {showExportBtn && (
+            <Button variation="primary" size="small">
+              <span className="flex align-baseline">
+                <span className="mr3">
+                  <IconDownload color="currentColor" />
+                </span>
+                Export
               </span>
-              Export
-            </span>
-          </Button>
+            </Button>
+          )}
         </div>
-        <Table
-          schema={this.props.tableSchema}
-          items={this.props.tableItems}
-          indexColumn
-        />
+
+        <Table schema={table && table.schema} items={table && table.items} />
+
         <Pagination
-          currentItemFrom={10}
-          currentItemTo={20}
-          textOf="of"
+          currentItemFrom={pagination.currentItemFrom}
+          currentItemTo={pagination.currentItemTo}
+          textOf={pagination.textOf}
           textShowRows="show rows"
           totalItems={32}
-          onNextClick={e => {
-            console.log(e)
-          }}
-          onPrevClick={e => {
-            console.log(e)
-          }}
+          onNextClick={this.handlePaginationNext}
+          onPrevClick={this.handlePaginationPrev}
         />
       </div>
     )
   }
 }
 
+ResourceList.defaultProps = {
+  exportBtn: { show: false },
+}
+
 ResourceList.propTypes = {
-  tableItems: PropTypes.array.isRequired,
-  tableSchema: PropTypes.object.isRequired,
+  table: PropTypes.shape({
+    items: PropTypes.array,
+    schema: PropTypes.object,
+  }).isRequired,
+  exportBtn: PropTypes.shape({
+    show: PropTypes.bool,
+  }),
+  pagination: PropTypes.shape({
+    onNextClick: PropTypes.func.isRequired,
+    onPrevClick: PropTypes.func.isRequired,
+    currentItemFrom: PropTypes.number.isRequired,
+    currentItemTo: PropTypes.number.isRequired,
+    textOf: PropTypes.string,
+  }),
 }
 
 export default ResourceList
