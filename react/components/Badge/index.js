d1b4307b6944feb661d8dc2136dbd8199ef2b5ec
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-08-22 16:46:07 -0300

    Update badge to receive semantic colors

--- react/components/Badge/index.js
+++ react/components/Badge/index.js
@@ -1,12 +1,26 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import config from 'vtex-tachyons/config.json'
 
 class Badge extends PureComponent {
   render() {
+    let theme = ''
+    switch (this.props.type) {
+      case 'success':
+        theme = 'bg-success c-on-success'
+        break
+      case 'error':
+        theme = 'bg-danger c-on-danger'
+        break
+      case 'warning':
+        theme = 'bg-warning c-on-warning'
+        break
+      default:
+        theme = 'bg-muted-4 c-on-base'
+    }
+
     return (
       <div
-        className="br-pill f6 pv2 ph3 dib fw5"
+        className={`br-pill f6 pv2 ph3 dib fw5 ${theme}`}
         style={{
           backgroundColor: this.props.bgColor,
           color: this.props.color,
@@ -18,12 +32,8 @@ class Badge extends PureComponent {
   }
 }
 
-Badge.defaultProps = {
-  color: config.colors['dark-gray'],
-  bgColor: config.colors['light-gray'],
-}
-
 Badge.propTypes = {
+  type: PropTypes.oneOf(['success', 'error', 'warning']),
   children: PropTypes.node.isRequired,
   color: PropTypes.string,
   bgColor: PropTypes.string,
