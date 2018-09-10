74f4feb4657c4c2093d03cb182f4211c8a355775
ohmyguigs <ohmyguigs@gmail.com>
2018-09-10 16:59:04 -0300

    Creates state to manage sort order by property

--- react/components/Table/index.js
+++ react/components/Table/index.js
@@ -1,8 +1,66 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Column, Table as VirtualTable, AutoSizer } from 'react-virtualized'
+import ArrowDown from '../icon/ArrowDown'
+import ArrowUp from '../icon/ArrowUp'
 
 class Table extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sortAsc: [],
+      sortDesc: [],
+      sortDefault: [],
+    }
+  }
+
+  componentDidMount() {
+    const { schema } = this.props
+    const sortAsc = []
+    const sortDesc = []
+    const sortDefault = []
+    Object.keys(schema.properties).forEach(key => {
+      if (schema.properties[key].sort) {
+        switch (schema.properties[key].sort.initialOrder) {
+          case 'ASC':
+            sortAsc.push(key)
+            break
+          case 'DESC':
+            sortDesc.push(key)
+            break
+          default: // no-order
+            sortDefault.push(key)
+            break
+        }
+      }
+    })
+    this.setState({ sortAsc, sortDesc, sortDefault })
+  }
+
+  toggleSortType = (key) => {
+    const { sortAsc, sortDesc, sortDefault } = this.state
+    if (sortAsc.includes(key)) {
+      this.removeFromArray(sortAsc, key)
+      const newSortDesc = sortDesc.concat([key])
+      this.setState({ sortAsc, sortDesc: newSortDesc })
+      return 'DESC'
+    } else if (sortDesc.includes(key)) {
+      this.removeFromArray(sortDesc, key)
+      const newSortDefault = sortDefault.concat([key])
+      this.setState({ sortDesc, sortDefault: newSortDefault })
+      return 'no-order'
+    }
+    this.removeFromArray(sortDefault, key)
+    const newSortAsc = sortAsc.concat([key])
+    this.setState({ sortAsc: newSortAsc, sortDefault })
+    return 'ASC'
+  }
+
+  removeFromArray = (arr, key) => {
+    const index = arr.indexOf(key)
+    arr.splice(index, 1)
+  }
+
   render() {
     const {
       schema,
@@ -14,6 +72,7 @@ class Table extends PureComponent {
       onRowMouseOver,
       onRowMouseOut,
     } = this.props
+    const { sortAsc, sortDesc } = this.state
     const properties = Object.keys(schema.properties)
     // hydrate items with index when 'indexColumn' prop is true
     const newItems = indexColumn
@@ -75,9 +134,26 @@ class Table extends PureComponent {
                     key={index}
                     headerRenderer={
                       headerRenderer ||
-                      function({ label }) {
-                        return <div className="truncate ph4">{label}</div>
-                      }
+                      (({ label }) => {
+                        return (
+                          <div className="truncate ph4">
+                            {schema.properties[key].sort
+                              ? <span className="pointer"
+                                onClick={() => {
+                                  const sortType = this.toggleSortType(key, schema.properties[key].sort)
+                                  schema.properties[key].sort.callback(sortType)
+                                }}>
+                                {`${label} `}{sortAsc.includes(key)
+                                  ? <ArrowDown size={11} />
+                                  : sortDesc.includes(key)
+                                    ? <ArrowUp size={11} />
+                                    : null}
+                              </span>
+                              : label
+                            }
+                          </div>
+                        )
+                      })
                     }
                     cellRenderer={
                       cellRenderer ||
