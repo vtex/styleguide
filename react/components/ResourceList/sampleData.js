bfd720fc08e3bf9b7b7b12626869994de5ec2247
ohmyguigs <ohmyguigs@gmail.com>
2018-09-20 18:50:17 -0300

    Handle initialy hidden fields

--- react/components/ResourceList/sampleData.js
+++ react/components/ResourceList/sampleData.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 import React from 'react'
 import Badge from '../Badge'
-import ArrowDown from '../icon/ArrowDown'
 
 const badgeData = [
   {
@@ -38,39 +37,10 @@ export default {
         type: 'number',
         title: 'Number',
       },
-    },
-  },
-  customSchema: {
-    properties: {
-      name: {
-        type: 'string',
-        title: 'Name',
-        headerRenderer: ({ label }) => {
-          return (
-            <div className="truncate ph4">
-              <span
-                className="pointer"
-                onClick={() =>
-                  alert(
-                    'You can customize header components. For example, call an external sort function',
-                  )
-                }
-              >
-                {`${label} `}
-                <ArrowDown size={11} />
-              </span>
-            </div>
-          )
-        },
-      },
-      email: {
-        type: 'string',
-        title: 'Email',
-        width: 35,
-      },
       color: {
         type: 'object',
         title: 'Color',
+        hidden: true,
         cellRenderer: ({ cellData }) => {
           return (
             <div className="mh4">
@@ -88,126 +58,151 @@ export default {
       email: 'olen.stamm21@yahoo.com',
       name: 'Patrick Rothfuss',
       number: 1.52725,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'junius0@gmail.com',
       name: 'Hurricane Skywalker IV',
       number: 2.84639,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'hailee.botsford24@gmail.com',
       name: 'Mr. Ron Smith',
       number: 3.23851,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'judd_gulgowski22@yahoo.com',
       name: 'Tom Braddy',
       number: 4.10182,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'gaetano.mayert71@yahoo.com',
       name: 'Norris Murazik',
       number: 5.33725,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'catharine.leuschke62@hotmail.com',
       name: 'Momochi Zabuza',
       number: 6.33245,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'candido_ryan@hotmail.com',
       name: 'Isaac Mayert IV',
       number: 7.96637,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'freda_ritchie26@yahoo.com',
       name: 'Dr. Lempi Mosciski',
       number: 8.42623,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'elissa28@gmail.com',
       name: 'Nikita Feeney',
       number: 9.11769,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'reginald1@yahoo.com',
       name: 'Janick Lesch',
       number: 10.85311,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'jerad_hyatt@hotmail.com',
       name: 'Golda Carter',
       number: 11.44288,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'jaquan.fisher61@yahoo.com',
       name: 'Dr. Cassandra Jerde',
       number: 12.73372,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'marlee_ziemann@yahoo.com',
       name: 'Kvothe Kshlerin',
       number: 13.62416,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'erman_Beier@yahoo.com',
       name: 'Arch Hegmann',
       number: 14.8212,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'arturo_schroeder30@yahoo.com',
       name: 'Alejandro Strosin',
       number: 15.73701,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'santos_doyle36@gmail.com',
       name: 'Zidane Vandervort',
       number: 16.26124,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'yvonne.gleason@hotmail.com',
       name: 'Tonny Balistreri',
       number: 17.79484,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'michaela51@hotmail.com',
       name: 'Emanuel Ullrich',
       number: 18.57879,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'erik_kuvalis41@hotmail.com',
       name: 'Beto',
       number: 19.87262,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'edwardo_kuvalis@hotmail.com',
       name: 'Mrs. Deion Beahan',
       number: 20.13464,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'alexa_ondricka45@hotmail.com',
       name: 'Trent Kerluke',
       number: 21.9677,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'darron_brakus63@yahoo.com',
       name: 'Jacquelyn Flatley Jr.',
       number: 22.55405,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'jamie.hermann60@gmail.com',
       name: 'Ervin Kuphal',
       number: 23.76965,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'cleo.klein@hotmail.com',
       name: 'Terrence Gutmann',
       number: 24.55731,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
     {
       email: 'Alicia.Ullrich@yahoo.com',
       name: 'Oren Beatty V',
       number: 25.6073,
+      color: badgeData[Math.floor(Math.random() * badgeData.length)],
     },
   ],
 }
