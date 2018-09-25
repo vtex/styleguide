14545c456910fee4d2b478c55d8e63d8de83e6dd
Lucas Bebber <lucasbbebber@gmail.com>
2018-09-25 16:49:16 -0300

    Remove commented out Button from toast

--- react/components/Toast/Toast.js
+++ react/components/Toast/Toast.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// import Button from '../Button'
 import CloseIcon from '../icon/Close'
 
 export default class Toast extends Component {
