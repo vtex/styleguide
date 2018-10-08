3c53e794850ac0cbe68284f32859daea939e0060
kevinch <kevin.chevallier@vtex.com.br>
2018-10-08 18:12:00 -0300

    Revert indexes

--- react/components/Dropdown/index.js
+++ react/components/Dropdown/index.js
@@ -1,3 +1,275 @@
-import Dropdown from './DropdownWithRef.js'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ArrowDownIcon from './ArrowDownIcon'
 
-export default Dropdown
+class Dropdown extends Component {
+  constructor(props) {
+    super(props)
+
+    // The initial value sent to the dropdown is kept in order to know
+    // whether or not to create and keep a first disabled option.
+    // If the initial value is invalid/empty, an empty option is kept
+    // so the select will have an empty state. Otherwise, the first
+    // value would automatically be chosen.
+    // However, you can't select a null/undefined option, so nil values
+    // are transformed to empty string.
+    this.initialValue = props.value == null ? '' : props.value
+  }
+
+  handleChange = e => {
+    const { disabled, onChange } = this.props
+    const {
+      target: { value },
+    } = e
+
+    !disabled && onChange && onChange(e, value)
+  };
+
+  getOptionFromValue = value => {
+    const { options } = this.props
+    const option = options.filter(option => option.value === value)[0]
+    if (!option) return null
+    return option
+  };
+
+  getDropdownIdentification = () => {
+    const { label, id, options } = this.props
+    if (label) {
+      return `Dropdown with the label "${label}"`
+    } else if (id) {
+      return `Dropdown #${id}`
+    }
+    return `Dropdown with the options ${options
+      .map(option => option.label)
+      .join(', ')}`
+  };
+
+  getPlaceholder = () => {
+    const { placeholder, label, helpText } = this.props
+    const placeholderValue = placeholder || label || helpText || ''
+
+    if (placeholderValue === '' && !this.sentPlaceholderWarning) {
+      console.warn(
+        `The following dropdown has a placeholder option, but no placeholder text. Please use at least one of these props: placeholder, label, or helpText.
+
+${this.getDropdownIdentification()}`
+      )
+
+      this.sentPlaceholderWarning = true
+    }
+
+    return placeholderValue
+  };
+
+  getSelectedOption = () => {
+    return this.getOptionFromValue(this.props.value)
+  };
+
+  getValueLabel() {
+    const selectedOption = this.getSelectedOption()
+    return selectedOption ? selectedOption.label : this.getPlaceholder()
+  }
+
+  render() {
+    const {
+      label,
+      id,
+      value,
+      size,
+      disabled,
+      options,
+      error,
+      errorMessage,
+      helpText,
+      placeholder,
+      preventTruncate,
+      autoFocus,
+      form,
+      name,
+      required,
+    } = this.props
+
+    const hasValidInitialValue = this.getOptionFromValue(this.initialValue) !==
+      null
+
+    const isPlaceholder = this.getSelectedOption() === null
+    let width
+    let iconSize
+
+    let classes = 'bg-transparent bn w-100 '
+    let containerClasses = 'br2 bw1 relative '
+    let selectClasses = 'o-0 absolute top-0 left-0 w-100 bottom-0 '
+
+    let labelClasses = 'vtex-dropdown__label dib mb3 w-100 '
+
+    const valueLabel = this.getValueLabel()
+    const showCaption = !valueLabel
+
+    classes += disabled ? 'bg-disabled ' : 'pointer '
+    selectClasses += disabled ? '' : 'pointer '
+    classes += !disabled && valueLabel
+      ? !isPlaceholder ? 'c-on-base ' : 'c-muted-2 '
+      : 'c-disabled '
+
+    switch (size) {
+      case 'large':
+        classes += 'f5 pv4 pl6 pr5 '
+        selectClasses += 'f5 '
+        labelClasses += 'f5 '
+        iconSize = 18
+        break
+      case 'x-large':
+        classes += 'f4 pv5 pl7 pr6 '
+        selectClasses += 'f4 '
+        labelClasses += 'f5 '
+        iconSize = 22
+        break
+      default:
+        classes += 'f6 pv3 pl5 pr4 '
+        selectClasses += 'f6 '
+        labelClasses += 'f6 '
+        iconSize = 16
+        break
+    }
+
+    const containerStyle = { width }
+
+    if (disabled) {
+      containerClasses += 'bg-disabled '
+    } else if (error || errorMessage) {
+      containerClasses += 'ba b--danger hover-b--danger '
+    } else {
+      containerClasses += 'bg-base hover-b--muted-3 ba b--muted-4 '
+    }
+
+    return (
+      <div className="vtex-dropdown">
+        <label>
+          {label && <span className={labelClasses}>{label}</span>}
+          <div className={containerClasses} style={containerStyle}>
+            <div id={id} className={`vtex-dropdown__button ${classes}`}>
+              <div className="flex">
+                <div className="vtex-dropdown__caption flex-auto tl truncate">
+                  {showCaption ? placeholder : valueLabel}
+                </div>
+                <div
+                  className={
+                    `vtex-dropdown__arrow flex-none flex items-center pl2 ${disabled ? 'c-on-disabled' : 'c-action-primary'}`
+                  }
+                >
+                  <ArrowDownIcon size={iconSize} />
+                </div>
+              </div>
+            </div>
+
+            <select
+              disabled={disabled}
+              className={selectClasses}
+              onChange={this.handleChange}
+              ref={this.props.forwardedRef}
+              {...{/* Check the comment on the constructor regarding nil values */}}
+              value={value == null ? '' : value}
+              autoFocus={autoFocus}
+              form={form}
+              name={name}
+              required={required}
+              style={{
+                // safari select height fix
+                WebkitAppearance: 'menulist-button',
+              }}
+            >
+              {/* iOS hack to optionally prevent truncating options */}
+              {preventTruncate && <optgroup label={label || helpText || ''} />}
+
+              {/* Creates a disabled first option in case the first value is invalid or empty */}
+              {(!hasValidInitialValue || placeholder) &&
+                <option
+                  disabled
+                  value={!hasValidInitialValue ? this.initialValue : null}
+                >
+                  {this.getPlaceholder()}
+                </option>}
+              {options.map(option => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </label>
+        {errorMessage &&
+          <div className="c-danger f6 mt3 lh-title">{errorMessage}</div>}
+        {helpText &&
+          <div className="c-muted-1 f6 mt3 lh-title">{helpText}</div>}
+      </div>
+    )
+  }
+}
+
+const DropdownWithRef = React.forwardRef((props, ref) => (
+  <Dropdown {...props} forwardedRef={ref} />
+))
+
+DropdownWithRef.displayName = 'Dropdown'
+
+DropdownWithRef.defaultProps = {
+  size: 'regular',
+  options: [],
+}
+
+DropdownWithRef.propTypes = {
+  /** Error highlight */
+  error: PropTypes.bool,
+  /** Error message */
+  errorMessage: PropTypes.node,
+  /** Help text */
+  helpText: PropTypes.node,
+  /** Dropdown label */
+  label: PropTypes.node,
+  /** Dropdown placeholder value */
+  placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Dropdown size */
+  size: PropTypes.oneOf(['regular', 'large', 'x-large']),
+  /** Dropdown options list */
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]).isRequired,
+      label: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]).isRequired,
+    })
+  ),
+  /** Prevent truncating large options texts on some devices/browsers, such as iOS */
+  preventTruncate: PropTypes.bool,
+  /** Spec attribute */
+  id: PropTypes.string,
+  /** Spec attribute */
+  autoFocus: PropTypes.bool,
+  /** Spec attribute */
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /** Spec attribute */
+  disabled: PropTypes.bool,
+  /** Spec attribute */
+  form: PropTypes.string,
+  /** Spec attribute */
+  name: PropTypes.string,
+  /** Spec attribute */
+  required: PropTypes.bool,
+  /** onChange event */
+  onChange: PropTypes.func,
+  /** onClose event */
+  onClose: PropTypes.func,
+  /** onOpen event */
+  onOpen: PropTypes.func,
+  /** Internal prop used for ref forwarding */
+  forwardedRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+}
+
+Dropdown.propTypes = DropdownWithRef.propTypes
+Dropdown.defaultProps = DropdownWithRef.defaultProps
+
+export default DropdownWithRef
--- react/components/Input/index.js
+++ react/components/Input/index.js
@@ -1,3 +1,314 @@
-import Input from './InputWithRef.js'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import config from 'vtex-tachyons/config.json'
 
-export default Input
+class Input extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false,
+    }
+  }
+
+  handleChange = event => {
+    this.props.onChange && this.props.onChange(event)
+  }
+
+  handleKeyPress = event => {
+    this.props.onKeyPress && this.props.onKeyPress(event)
+  }
+
+  handleKeyDown = event => {
+    this.props.onKeyDown && this.props.onKeyDown(event)
+  }
+
+  handleKeyUp = event => {
+    this.props.onKeyUp && this.props.onKeyUp(event)
+  }
+
+  handleFocus = event => {
+    this.setState({ active: true })
+    this.props.onFocus && this.props.onFocus(event)
+  }
+
+  handleBlur = event => {
+    this.setState({ active: false })
+    this.props.onBlur && this.props.onBlur(event)
+  }
+
+  render() {
+    const {
+      errorMessage,
+      error,
+      label,
+      size,
+      token,
+      helpText,
+      dataAttributes,
+      prefix,
+      suffixIcon,
+      groupBottom,
+    } = this.props
+    const { active } = this.state
+
+    const dataAttrs = {}
+    for (const key of Object.keys(dataAttributes)) {
+      dataAttrs[`data-${key}`] = dataAttributes[key]
+    }
+
+    const widthClass = 'w-100'
+    const box = 'ma0 border-box'
+    const borderRadius = `br2 ${groupBottom ? 'br--top' : ''}`
+    const border = `bw1 ${borderRadius} b--solid outline-0`
+    // On bw1 change, update config.borderRadius[1] below accordingly
+
+    const topBottomHeight = config.borderRadius[1] * 2 // 2 is top AND BOTTOM
+    const prefixAndSuffixPosition = `${config.borderRadius[1]}rem`
+    const calcPrefixAndSuffixHeight = `calc(100% - ${topBottomHeight}rem)`
+    const typography = 'c-on-base'
+    let classes = `${widthClass} ${box} ${border} ${typography} `
+
+    let labelClasses = 'vtex-input__label db mb3 w-100 '
+
+    let prefixAndSuffixClasses =
+      'vtex-input__prefix absolute c-muted-1 fw5 flex items-center '
+
+    if (token) {
+      classes += 'code '
+    }
+
+    if (this.props.disabled) {
+      classes += 'bg-disabled b--disabled c-disabled '
+    } else {
+      classes += 'bg-base '
+
+      if (error || errorMessage) {
+        classes += 'b--danger hover-b--danger '
+      } else if (active) {
+        classes += 'b--muted-2 '
+      } else {
+        classes += 'b--muted-4 hover-b--muted-3 '
+      }
+    }
+
+    switch (size) {
+      case 'large':
+        classes += `f5 pv4 ${prefix ? 'pl8 pr6' : 'ph6'} ${
+          suffixIcon ? 'pr8' : ''
+        }`
+        labelClasses += 'f5 '
+        prefixAndSuffixClasses += 'ph4 f5'
+        // iconSize = 18
+        break
+      case 'x-large':
+        classes += `f4 pv5 ${prefix ? 'pl8 pr7' : 'ph7'} ${
+          suffixIcon ? 'pr8' : ''
+        }`
+        labelClasses += 'f5 '
+        prefixAndSuffixClasses += 'ph5 f4 '
+        // iconSize = 22
+        break
+      default:
+        classes += `f6 pv3 ${prefix ? 'pl7 pr5' : 'ph5'} ${
+          suffixIcon ? 'pr7' : ''
+        }`
+        labelClasses += 'f6 '
+        prefixAndSuffixClasses += 'ph3 fw5 f6 '
+        // iconSize = 16
+        break
+    }
+
+    return (
+      <label className="vtex-input w-100">
+        {label && <span className={labelClasses}>{label}</span>}
+        <div className="flex vtex-input-prefix__group relative">
+          {prefix && (
+            <span
+              style={{
+                height: calcPrefixAndSuffixHeight,
+                top: prefixAndSuffixPosition,
+                left: prefixAndSuffixPosition,
+              }}
+              className={prefixAndSuffixClasses}
+            >
+              {prefix}
+            </span>
+          )}
+          <input
+            {...dataAttrs}
+            ref={this.props.forwardedRef}
+            onBlur={this.handleBlur}
+            onFocus={this.handleFocus}
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            onKeyDown={this.handleKeyDown}
+            onKeyUp={this.handleKeyUp}
+            className={classes}
+            disabled={this.props.disabled}
+            accept={this.props.accept}
+            autoComplete={this.props.autoComplete}
+            autoCorrect={this.props.autoCorrect}
+            autoFocus={this.props.autoFocus}
+            autoSave={this.props.autoSave}
+            defaultValue={this.props.defaultValue}
+            inputMode={this.props.inputMode}
+            list={this.props.list}
+            max={this.props.max}
+            maxLength={this.props.maxLength}
+            min={this.props.min}
+            minLength={this.props.minLength}
+            multiple={this.props.multiple}
+            name={this.props.name}
+            pattern={this.props.pattern}
+            placeholder={this.props.placeholder}
+            readOnly={this.props.readOnly}
+            required={this.props.required}
+            spellCheck={this.props.spellCheck}
+            src={this.props.src}
+            step={this.props.step}
+            tabIndex={this.props.tabIndex}
+            type={this.props.type}
+            value={this.props.value}
+            id={this.props.id}
+            style={{
+              WebkitAppearance: 'none',
+            }}
+          />
+          {suffixIcon && (
+            <span
+              style={{
+                height: calcPrefixAndSuffixHeight,
+                top: prefixAndSuffixPosition,
+                right: prefixAndSuffixPosition,
+              }}
+              className={prefixAndSuffixClasses}
+            >
+              {suffixIcon}
+            </span>
+          )}
+        </div>
+        {errorMessage && (
+          <div className="c-danger f6 mt3 lh-title">{errorMessage}</div>
+        )}
+        {helpText && (
+          <div className="c-muted-1 f6 mt3 lh-title">{helpText}</div>
+        )}
+      </label>
+    )
+  }
+}
+
+const InputWithRef = React.forwardRef((props, ref) => (
+  <Input {...props} forwardedRef={ref} />
+))
+
+InputWithRef.displayName = 'Input'
+
+InputWithRef.defaultProps = {
+  autoFocus: false,
+  token: false,
+  dataAttributes: {},
+  disabled: false,
+  label: '',
+  multiple: false,
+  readOnly: false,
+  error: false,
+  size: 'regular',
+  prefix: '',
+  type: 'text',
+}
+
+InputWithRef.propTypes = {
+  /** Error highlight */
+  error: PropTypes.bool,
+  /** Error message */
+  errorMessage: PropTypes.string,
+  /** If the input is an API Key, App Key or App Token */
+  token: PropTypes.bool,
+  /** Help text */
+  helpText: PropTypes.node,
+  /** Input size */
+  size: PropTypes.oneOf(['regular', 'large', 'x-large']),
+  /** Label */
+  label: PropTypes.string,
+  /** Prefix */
+  prefix: PropTypes.string,
+  /** Internal prop used for ref forwarding */
+  forwardedRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  /** Spec attribute */
+  accept: PropTypes.string,
+  /** Spec attribute */
+  disabled: PropTypes.bool,
+  /** Spec attribute */
+  autoComplete: PropTypes.string,
+  /** Spec attribute */
+  autoCorrect: PropTypes.string,
+  /** Spec attribute */
+  autoFocus: PropTypes.bool,
+  /** Spec attribute */
+  autoSave: PropTypes.string,
+  /** List of data attributes as a object like `{'locale': 'en-US'}` */
+  dataAttributes: PropTypes.object,
+  /** Spec attribute */
+  defaultValue: PropTypes.string,
+  /** Whether the border should join with an element below */
+  groupBottom: PropTypes.bool,
+  /** Spec attribute */
+  id: PropTypes.string,
+  /** Spec attribute */
+  inputMode: PropTypes.string,
+  /** Spec attribute */
+  list: PropTypes.string,
+  /** Spec attribute */
+  max: PropTypes.string,
+  /** Spec attribute */
+  maxLength: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /** Spec attribute */
+  min: PropTypes.string,
+  /** Spec attribute */
+  minLength: PropTypes.string,
+  /** Spec attribute */
+  multiple: PropTypes.bool,
+  /** Spec attribute */
+  name: PropTypes.string,
+  /** Spec attribute */
+  pattern: PropTypes.string,
+  /** Spec attribute */
+  placeholder: PropTypes.string,
+  /** Spec attribute */
+  readOnly: PropTypes.bool,
+  /** Spec attribute */
+  required: PropTypes.bool,
+  /** Spec attribute */
+  spellCheck: PropTypes.string,
+  /** Spec attribute */
+  src: PropTypes.string,
+  /** Spec attribute */
+  step: PropTypes.string,
+  /** SuffixIcon attribute */
+  suffixIcon: PropTypes.element,
+  /** Spec attribute */
+  tabIndex: PropTypes.string,
+  /** Spec attribute */
+  type: PropTypes.string,
+  /** Spec attribute */
+  value: PropTypes.string,
+  /** onChange event */
+  onChange: PropTypes.func,
+  /** onKeyDown event */
+  onKeyDown: PropTypes.func,
+  /** onKeyPress event */
+  onKeyPress: PropTypes.func,
+  /** onKeyUp event */
+  onKeyUp: PropTypes.func,
+  /** onFocus event */
+  onFocus: PropTypes.func,
+  /** onBlur event */
+  onBlur: PropTypes.func,
+}
+
+Input.propTypes = InputWithRef.propTypes
+Input.defaultProps = InputWithRef.defaultProps
+
+export default InputWithRef
