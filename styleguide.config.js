53885e2f5098535f7126c343454750c77891285d
Tas <tas.g.maia@gmail.com>
2018-10-11 16:36:25 -0300

    Add breakpoints to styles

--- styleguide.config.js
+++ styleguide.config.js
@@ -50,6 +50,10 @@ module.exports = {
           content: 'docs/styles/introduction.md',
         },
         {
+          name: 'Breakpoints',
+          content: 'docs/styles/breakpoints.md',
+        },
+        {
           name: 'Colors',
           content: 'docs/styles/colors.md',
         },
