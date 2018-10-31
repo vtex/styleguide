e1aa8a96b5db39a84af2ddef34bce06e55bf1d7e
Lucas Bebber <lucasbbebber@gmail.com>
2018-10-31 14:10:54 -0300

    Update spinner to support transitioning between states

--- react/components/Spinner/index.js
+++ react/components/Spinner/index.js
@@ -5,58 +5,121 @@ import { baseClassname } from '../icon/utils'
 const radius = 40
 const circ = 2 * radius * Math.PI
 
-const Spinner = ({ color, size, block }) => (
-  <svg
-    className={`${baseClassname('spinner')} ${
-      !color ? 'c-action-primary' : ''
-    } ${block ? 'db' : ''}`}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 100 100"
-    preserveAspectRatio="xMidYMid"
-    height={size}
-    width={size}>
-    <circle
-      cx="50"
-      cy="50"
-      fill="none"
-      r={radius}
-      stroke={color || 'currentColor'}
-      strokeWidth="10"
-      strokeDasharray={`0 0 2 ${circ}`}
-      strokeLinecap="round"
-      strokeDashoffset="1"
-      transform="rotate(96 50 50)">
-      <animate
-        attributeName="stroke-dasharray"
-        dur="1.5s"
-        repeatCount="indefinite"
-        values={`0 0 2 ${circ};
+const WORKING = 'working'
+const IDLE = 'idle'
+
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.strokeAnim = React.createRef()
+
+    this.state = {
+      animating: props.status === WORKING,
+    }
+  }
+
+  onNextLoop = callback => {
+    this.strokeAnim.current.onrepeat = () => {
+      callback()
+      this.strokeAnim.current.onrepeat = null
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { status } = this.props
+    const { status: prevStatus } = prevProps
+
+    if (status !== prevStatus) {
+      if (status === IDLE) {
+        this.onNextLoop(() => {
+          this.setState({
+            animating: false,
+          })
+        })
+      } else if (status === WORKING) {
+        this.onNextLoop(() => {
+          this.setState({
+            animating: true,
+          })
+        })
+      }
+    }
+  }
+
+  render() {
+    const { color, size, block } = this.props
+    const { animating } = this.state
+
+    return (
+      <svg
+        className={
+          `${baseClassname('spinner')} ${!color ? 'c-action-primary' : ''} ${block ? 'db' : ''}`
+        }
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+        height={size}
+        width={size}
+      >
+        <g visibility={animating ? 'inherit' : 'hidden'}>
+          <circle
+            cx="50"
+            cy="50"
+            fill="none"
+            r={radius}
+            stroke={color || 'currentColor'}
+            strokeWidth="10"
+            strokeDasharray={`0 0 2 ${circ}`}
+            strokeLinecap="round"
+            strokeDashoffset="1"
+            transform="rotate(-90 50 50)"
+          >
+            <animate
+              attributeName="stroke-dasharray"
+              dur="1.25s"
+              calcMode="spline"
+              keyTimes="0;0.5;1"
+              keySplines="0.455, 0.030, 0.515, 0.955; 0.455, 0.030, 0.515, 0.955"
+              repeatCount="indefinite"
+              values={
+                `0 0 2 ${circ};
           0 0 ${circ * 0.75} ${circ};
-          0 ${circ - 2} ${circ * 0.75} ${circ}`}
-      />
-      <animateTransform
-        attributeName="transform"
-        type="rotate"
-        calcMode="linear"
-        values="0 50 50;360 50 50"
-        keyTimes="0;1"
-        dur="0.75s"
-        begin="0s"
-        repeatCount="indefinite"
-      />
-    </circle>
-  </svg>
-)
+          0 ${circ - 2} ${circ * 0.75} ${circ}`
+              }
+              ref={this.strokeAnim}
+            />
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              calcMode="linear"
+              values="-90 50 50;275 50 50"
+              keyTimes="0;1"
+              dur="0.625s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </g>
+      </svg>
+    )
+  }
+}
 
 Spinner.propTypes = {
-  block: PropTypes.bool,
+  /** Color of the spinner */
   color: PropTypes.string,
+  /** Size (diameter) of the spinner */
   size: PropTypes.number,
+  /** Status of the spinner; used for smooth transitioning between different states */
+  status: PropTypes.oneOf(['working', 'idle']),
+  /** Sets the display to block */
+  block: PropTypes.bool,
 }
 
 Spinner.defaultProps = {
   block: false,
   size: 40,
+  status: WORKING,
 }
 
 export default Spinner
