50b79577e2b34e3bf857590af9282bbee67b7d9f
Thiago Murakami <thiago.murakami@gmail.com>
2019-01-08 10:17:21 -0200

    Fix arrow icons

--- react/components/Select/DropdownIndicator.js
+++ react/components/Select/DropdownIndicator.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import COLORS from './colors'
-import IconCaretDown from '../icon/CaretDown'
-import IconCaretUp from '../icon/CaretUp'
+import ArrowDownIcon from '../Dropdown/ArrowDownIcon'
+import ArrowUpIcon from './ArrowUpIcon'
 
 const DropdownIndicator = ({ innerProps, selectProps }) => {
   const arrowColor = selectProps.isDisabled ? COLORS.gray : COLORS.blue
   return (
     <div className="flex items-center h-100 pr4 pointer" {...innerProps}>
       {selectProps.menuIsOpen ? (
-        <IconCaretUp color={arrowColor} size={18} />
+        <ArrowUpIcon color={arrowColor} size={18} />
       ) : (
-        <IconCaretDown color={arrowColor} size={18} />
+        <ArrowDownIcon color={arrowColor} size={18} />
       )}
     </div>
   )
