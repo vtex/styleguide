aa90f5d54ae3be755b412e7642b34596f9b9eef4
ericreis <eric.figueiredo@vtex.com.br>
2019-04-16 19:08:18 -0300

    Add ButtonWithAction component

---
+++ react/components/EXPERIMENTAL_ButtonWithAction/index.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ActionMenu from '../ActionMenu'
+import ButtonWithIcon from '../ButtonWithIcon'
+
+/**
+ * @visibleName ButtonWithAction
+ */
+function EXPERIMENTAL_ButtonWithAction(props) {
+  console.warn(
+    `Experimental component warning:
+
+      ButtonWithAction is in an experimental state.
+      This component may suffer breaking changes in a near future, even in minor or patch versions.
+      It may even cease to exist without further notice 👻`
+  )
+
+  const { actions, ...buttonProps } = props
+  return (
+    <div className="flex">
+      <ButtonWithIcon {...buttonProps} groupRight />
+      <ActionMenu options={actions} groupLeft />
+    </div>
+  )
+}
+
+EXPERIMENTAL_ButtonWithAction.propTypes = {
+  actions: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      handleCallback: PropTypes.func,
+      toggle: PropTypes.shape({
+        checked: PropTypes.bool,
+        semantic: PropTypes.bool,
+      }),
+    })
+  ).isRequired,
+}
+
+export default EXPERIMENTAL_ButtonWithAction
