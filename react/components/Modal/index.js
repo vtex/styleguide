418176855dde203a1b792dbb7bcd6fce62ac013b
Lurian <luclurian@gmail.com>
2019-05-08 11:10:37 -0300

    Remove hooks from the Modal component

--- react/components/Modal/index.js
+++ react/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import ResponsiveModal from 'react-responsive-modal'
 import TopBar from './TopBar'
@@ -6,93 +6,98 @@ import BottomBar from './BottomBar'
 import { mh100, scrollBar } from './global.css'
 import './modal.global.css'
 
-const Modal = props => {
-  const {
-    isOpen,
-    centered,
-    onClose,
-    closeOnEsc,
-    closeOnOverlayClick,
-    showCloseIcon,
-    bottomBar,
-    title,
-    children,
-    responsiveFullScreen,
-  } = props
-
-  const [shadowBottom, setShadowBottom] = useState(false)
-  const [shadowTop, setShadowTop] = useState(false)
-
-  const handleScroll = event => {
-    const element = event.target
-    if (element.scrollTop === 0) setShadowTop(false)
-    else setShadowTop(true)
-
-    if (element.scrollHeight - element.scrollTop === element.clientHeight)
-      setShadowBottom(false)
-    else setShadowBottom(true)
+class Modal extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { shadowBottom: false, shadowTop: false }
   }
 
-  return (
-    <ResponsiveModal
-      open={isOpen}
-      little={centered}
-      onClose={onClose}
-      closeOnEsc={closeOnEsc}
-      closeOnOverlayClick={closeOnOverlayClick}
-      showCloseIcon={false && showCloseIcon}
-      classNames={{
-        overlay: `vtex-modal__overlay ${
-          responsiveFullScreen ? 'pa5-ns pa0' : ''
-        }`,
-        modal: `vtex-modal__modal ${
-          responsiveFullScreen
-            ? 'br2-ns w-100 w-auto-ns h-100 h-auto-ns'
-            : 'br2'
-        } ${mh100} flex flex-column`,
-        closeIcon: 'vtex-modal__close-icon',
-      }}
-      styles={{
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.8)',
-          zIndex: 10000,
-        },
-        modal: {
-          padding: 0,
-        },
-        closeIcon: {
-          top: '8px',
-          right: '8px',
-          padding: '10px',
-        },
-      }}
-      closeIconSize={18}>
-      <TopBar
-        title={title}
+  handleScroll = event => {
+    const element = event.target
+    if (element.scrollTop === 0) this.setState({ shadowTop: false })
+    else this.setState({ shadowTop: true })
+
+    if (element.scrollHeight - element.scrollTop === element.clientHeight)
+      this.setState({ shadowBottom: false })
+    else this.setState({ shadowBottom: true })
+  }
+
+  render = () => {
+    const {
+      isOpen,
+      centered,
+      onClose,
+      closeOnEsc,
+      closeOnOverlayClick,
+      showCloseIcon,
+      bottomBar,
+      title,
+      children,
+      responsiveFullScreen,
+    } = this.props
+    const { shadowBottom, shadowTop } = this.state
+
+    return (
+      <ResponsiveModal
+        open={isOpen}
+        little={centered}
         onClose={onClose}
-        showBottomShadow={shadowTop}
-        responsiveFullScreen={responsiveFullScreen}
-      />
-      <div
-        className={`${
-          responsiveFullScreen ? 'ph7 ph8-ns' : 'ph8'
-        } overflow-auto flex-shrink-1 flex-grow-1 ${
-          bottomBar ? '' : 'pb8'
-        } ${scrollBar}`}
-        onScroll={handleScroll}>
-        {children}
-      </div>
-      {bottomBar ? (
-        <BottomBar
-          showTopShadow={shadowBottom}
-          responsiveFullScreen={responsiveFullScreen}>
-          {bottomBar}
-        </BottomBar>
-      ) : (
-        ''
-      )}
-    </ResponsiveModal>
-  )
+        closeOnEsc={closeOnEsc}
+        closeOnOverlayClick={closeOnOverlayClick}
+        showCloseIcon={false && showCloseIcon}
+        classNames={{
+          overlay: `vtex-modal__overlay ${
+            responsiveFullScreen ? 'pa5-ns pa0' : ''
+          }`,
+          modal: `vtex-modal__modal ${
+            responsiveFullScreen
+              ? 'br2-ns w-100 w-auto-ns h-100 h-auto-ns'
+              : 'br2'
+          } ${mh100} flex flex-column`,
+          closeIcon: 'vtex-modal__close-icon',
+        }}
+        styles={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            zIndex: 10000,
+          },
+          modal: {
+            padding: 0,
+          },
+          closeIcon: {
+            top: '8px',
+            right: '8px',
+            padding: '10px',
+          },
+        }}
+        closeIconSize={18}>
+        <TopBar
+          title={title}
+          onClose={onClose}
+          showBottomShadow={shadowTop}
+          responsiveFullScreen={responsiveFullScreen}
+        />
+        <div
+          className={`${
+            responsiveFullScreen ? 'ph7 ph8-ns' : 'ph8'
+          } overflow-auto flex-shrink-1 flex-grow-1 ${
+            bottomBar ? '' : 'pb8'
+          } ${scrollBar}`}
+          onScroll={this.handleScroll}>
+          {children}
+        </div>
+        {bottomBar ? (
+          <BottomBar
+            showTopShadow={shadowBottom}
+            responsiveFullScreen={responsiveFullScreen}>
+            {bottomBar}
+          </BottomBar>
+        ) : (
+          ''
+        )}
+      </ResponsiveModal>
+    )
+  }
 }
 
 Modal.defaultProps = {
