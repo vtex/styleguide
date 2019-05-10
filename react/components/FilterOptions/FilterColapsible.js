696d296eb8f51af38642ed458dba9f682491f831
Lurian <luclurian@gmail.com>
2019-05-09 23:18:26 -0300

    Refact component to have only the prop as the source of truth

--- react/components/FilterOptions/FilterColapsible.js
+++ react/components/FilterOptions/FilterColapsible.js
@@ -1,69 +1,26 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import merge from 'lodash/merge'
 
 import Statement from '../UncoupledStatement/UncoupledStatement'
 import Collapsible from '../Collapsible'
 
-const emptyVirtualStatement = {
-  subject: null,
-  verb: null,
-  object: null,
-  error: null,
-}
-
-const filterStatementBySubject = (statements = [], subject, options = {}) => {
-  const hasStatement = statements.some(st => st.subject === subject)
-  return hasStatement
-    ? statements.filter(st => st.subject === subject)
-    : [
-        {
-          ...emptyVirtualStatement,
-          subject: subject,
-          verb:
-            options[subject] && options[subject].verbs.length > 0
-              ? options[subject].verbs[0].value
-              : null,
-        },
-      ]
-}
-
 class FilterColapsible extends PureComponent {
   constructor(props) {
     super(props)
-    this.filterMenuContainer = React.createRef()
-
     this.state = {
       isCollapsibleOpen: false,
-      virtualStatement: filterStatementBySubject(
-        props.statements,
-        props.subject,
-        props.options
-      )[0],
     }
   }
 
   handleChangeStatement = (newValue, structure) => {
-    this.setState(state => {
-      const virtualStatement = merge({}, state.virtualStatement, {
-        [structure]: newValue,
-      })
-      this.props.onChangeStatement(virtualStatement)
-      return { virtualStatement }
-    })
-  }
-
-  resetVirtualStatement = () => {
-    const { subject, options } = this.props
-    const statement = filterStatementBySubject([], subject, options)
-    this.setState({ virtualStatement: statement })
+    const { statement } = this.props
+    return this.props.onChangeStatement({ ...statement, [structure]: newValue })
   }
 
   render() {
-    const { options, subject, statements } = this.props
-    const { isCollapsibleOpen, virtualStatement } = this.state
+    const { options, subject, statement } = this.props
+    const { isCollapsibleOpen } = this.state
 
-    const statement = filterStatementBySubject(statements, subject)[0]
     const shouldOmitVerb = options[subject].verbs.length === 1
 
     return (
@@ -88,7 +45,7 @@ class FilterColapsible extends PureComponent {
                 omitVerbs={shouldOmitVerb}
                 options={options}
                 subjectPlaceholder={'…'}
-                statements={[merge({}, statement, virtualStatement)]}
+                statements={[statement]}
                 onChangeStatement={this.handleChangeStatement}
                 onChangeObjectCallback={value =>
                   this.handleChangeStatement(value, 'object')
@@ -102,10 +59,26 @@ class FilterColapsible extends PureComponent {
   }
 }
 
+FilterColapsible.defaultProps = {
+  statement: {
+    subject: null,
+    verb: null,
+    object: null,
+    error: null,
+  },
+}
+
 FilterColapsible.propTypes = {
   options: PropTypes.object.isRequired,
-  subject: PropTypes.string,
-  statements: PropTypes.array,
+  subject: PropTypes.string.isRequired,
+  statement: PropTypes.objectOf(
+    PropTypes.shape({
+      subject: PropTypes.string,
+      verb: PropTypes.string.isRequired,
+      object: PropTypes.object,
+      error: PropTypes.any,
+    })
+  ),
   onChangeStatement: PropTypes.func.isRequired,
 }
 
--- react/components/FilterOptions/index.js
+++ react/components/FilterOptions/index.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import find from 'lodash/find'
+import head from 'lodash/head'
 
 import FilterColapsible from './FilterColapsible'
 
@@ -11,6 +13,21 @@ class FilterOptions extends PureComponent {
     this.props.onChangeStatements([...filteredStatements, statement])
   }
 
+  extractStatement = (statements, subject) =>
+    find(statements, st => st.subject === subject)
+
+  createEmptyStatement = (subject, options) => {
+    const availableVerbs =
+      options && options[subject] ? options[subject].verbs : []
+    const verb = head(availableVerbs) ? head(availableVerbs).value : null
+    return {
+      subject,
+      verb,
+      object: null,
+      error: null,
+    }
+  }
+
   render() {
     const { options, statements } = this.props
     const optionsKeys = Object.keys(options)
@@ -18,12 +35,15 @@ class FilterOptions extends PureComponent {
       optionsKeys.length > 0 && (
         <div className={'flex flex-column'}>
           {optionsKeys.map(subject => {
+            const statement =
+              this.extractStatement(statements, subject) ||
+              this.createEmptyStatement(subject, options)
             return (
-              <div key={`VTEX__filter_option--${subject}`} className="ma2">
+              <div key={subject} className="ma2">
                 <FilterColapsible
                   subject={subject}
                   options={options}
-                  statements={statements}
+                  statement={statement}
                   onChangeStatement={this.handleStatementChange}
                 />
               </div>
@@ -41,9 +61,21 @@ FilterOptions.defaultProps = {
 
 FilterOptions.propTypes = {
   /** filter options (mirroring statements from Conditions component) */
-  options: PropTypes.object.isRequired,
+  options: PropTypes.objectOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      verbs: PropTypes.array.isRequired,
+    })
+  ).isRequired,
   /** filter statements (mirroring statements from Conditions component) */
-  statements: PropTypes.array,
+  statements: PropTypes.arrayOf(
+    PropTypes.shape({
+      subject: PropTypes.string,
+      verb: PropTypes.string.isRequired,
+      object: PropTypes.any,
+      error: PropTypes.any,
+    })
+  ),
   /** Filters change callback: returns array of statement definitions */
   onChangeStatements: PropTypes.func.isRequired,
 }
