ff854eb88d1e60de9ba066fe5e0586fc6c4a9452
Lurian <luclurian@gmail.com>
2019-05-09 23:21:10 -0300

    Rename UncoupledStatement to index for neat imports

--- react/components/FilterOptions/FilterColapsible.js
+++ react/components/FilterOptions/FilterColapsible.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import Statement from '../UncoupledStatement/UncoupledStatement'
+import Statement from '../UncoupledStatement'
 import Collapsible from '../Collapsible'
 
 class FilterColapsible extends PureComponent {
