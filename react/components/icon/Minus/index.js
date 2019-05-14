3dcd9ea7746e16eed53a14f8c993084ce5d3eb80
samuraiexx <samuraiexx@gmail.com>
2019-05-14 17:45:31 -0300

    Add solid prop to Minus Icon

--- react/components/icon/Minus/index.js
+++ react/components/icon/Minus/index.js
@@ -9,10 +9,26 @@ const iconBase = {
 
 class Minus extends PureComponent {
   render() {
-    const { color, size, block } = this.props
+    const { color, size, block, solid } = this.props
     const newSize = calcIconSize(iconBase, size)
 
-    return (
+    return solid ? (
+      <svg
+        className={`${baseClassname('minus')} ${block ? 'db' : ''}`}
+        width={newSize.width}
+        height={newSize.height}
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          xmlns="http://www.w3.org/2000/svg"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8 16C12.418 16 16 12.4185 16 8C16 3.58154 12.418 0 8 0C3.58203 0 0 3.58154 0 8C0 12.4185 3.58203 16 8 16ZM5 7C4.44727 7 4 7.44775 4 8C4 8.55225 4.44727 9 5 9H11C11.5527 9 12 8.55225 12 8C12 7.44775 11.5527 7 11 7H5Z"
+          fill={color}
+        />
+      </svg>
+    ) : (
       <svg
         className={`${baseClassname('minus')} ${block ? 'db' : ''}`}
         width={newSize.width}
@@ -34,10 +50,12 @@ Minus.defaultProps = {
   color: 'currentColor',
   size: 16,
   block: false,
+  solid: false,
 }
 
 Minus.propTypes = {
   color: PropTypes.string,
+  solid: PropTypes.bool,
   size: PropTypes.number,
   block: PropTypes.bool,
 }
