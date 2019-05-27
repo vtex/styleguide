5dcdb682a33ab4e3b5962293d3f61ea784b85985
Luciano Júnior <luciannojunior@gmail.com>
2019-05-27 11:46:30 -0300

    Fixes Checkbox with big label

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- **CheckBox** breaks when the label has more than one line.
 
 ## [8.58.0] - 2019-07-04
 
--- react/components/Checkbox/index.js
+++ react/components/Checkbox/index.js
@@ -24,58 +24,60 @@ class Checkbox extends PureComponent {
         className={classNames('flex items-center relative', {
           pointer: !disabled,
         })}>
-        <div
-          className={classNames('h1 w1 relative ba bw1 br1 ', {
-            'b--muted-4 pointer': !checked && !disabled,
-            'b--disabled bg-muted-5 c-disabled': !checked && disabled,
-            'b--action-primary bg-action-primary': checked && !disabled,
-            'b--muted-5 bg-muted-5': partial && !checked && disabled,
-            'b--disabled bg-disabled': checked && disabled,
-            mr3: label,
-          })}
-          style={{
-            transition: 'background 20ms, border 100ms',
-            backgroundColor: partial && !checked && !disabled && '#dbe9fd',
-            borderColor: partial && !checked && !disabled && '#dbe9fd',
-          }}
-        />
-        <div
-          className="absolute w1 h1 flex o-100"
-          style={{
-            left: 0,
-            top: label && 1, // The lack of label creates a small gap
-          }}>
+        <div className={classNames('relative mr3 w1 h1', { mr3: label })}>
           <div
-            className={`absolute top-0 left-0 bottom-0 overflow-hidden w-100 flex items-center align-center justify-center ${
-              disabled ? 'c-on-disabled' : 'c-on-action-primary'
-            }`}
-            style={{ transition: 'right 110ms ease-in-out 30ms' }}>
-            {checked ? (
-              <CheckIcon size={12} color="currentColor" />
-            ) : (
-              partial && (
-                <CheckPartial
-                  size={12}
-                  color={disabled ? '#979899' : '#1346d8'}
-                />
-              )
-            )}
+            className={classNames('h1 w1 absolute ba bw1 br1 ', {
+              'b--muted-4 pointer': !checked && !disabled,
+              'b--disabled bg-muted-5 c-disabled': !checked && disabled,
+              'b--action-primary bg-action-primary': checked && !disabled,
+              'b--muted-5 bg-muted-5': partial && !checked && disabled,
+              'b--disabled bg-disabled': checked && disabled,
+              mr3: label,
+            })}
+            style={{
+              transition: 'background 20ms, border 100ms',
+              backgroundColor: partial && !checked && !disabled && '#dbe9fd',
+              borderColor: partial && !checked && !disabled && '#dbe9fd',
+            }}
+          />
+          <div
+            className="absolute w1 h1 flex o-100"
+            style={{
+              left: 0,
+              top: label && 1, // The lack of label creates a small gap
+            }}>
+            <div
+              className={`absolute top-0 left-0 bottom-0 overflow-hidden w-100 flex items-center align-center justify-center ${
+                disabled ? 'c-on-disabled' : 'c-on-action-primary'
+              }`}
+              style={{ transition: 'right 110ms ease-in-out 30ms' }}>
+              {checked ? (
+                <CheckIcon size={12} color="currentColor" />
+              ) : (
+                partial && (
+                  <CheckPartial
+                    size={12}
+                    color={disabled ? '#979899' : '#1346d8'}
+                  />
+                )
+              )}
+            </div>
           </div>
+          <input
+            checked={checked}
+            className={classNames('h1 w1 absolute o-0', {
+              pointer: !disabled,
+            })}
+            disabled={disabled}
+            required={required}
+            id={id}
+            name={name}
+            onChange={this.handleChange}
+            type="checkbox"
+            value={value}
+            tabIndex={0}
+          />
         </div>
-        <input
-          checked={checked}
-          className={classNames('h1 w1 absolute o-0', {
-            pointer: !disabled,
-          })}
-          disabled={disabled}
-          required={required}
-          id={id}
-          name={name}
-          onChange={this.handleChange}
-          type="checkbox"
-          value={value}
-          tabIndex={0}
-        />
         {label && (
           <label
             className={classNames(
