ee7531436978897ae7bec1d4cd98543b31ded39d
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-05 17:05:56 -0300

    Add icons to image and underline

--- react/components/icon/Image/index.js
+++ react/components/icon/Image/index.js
@@ -7,38 +7,42 @@ const iconBase = {
   height: 12,
 }
 
-class Italic extends PureComponent {
+class Image extends PureComponent {
   render() {
     const { color, size, block } = this.props
     const newSize = calcIconSize(iconBase, size)
 
     return (
       <svg
-        className={`${baseClassname('italic')} ${block ? 'db' : ''}`}
+        className={`${baseClassname('image')} ${block ? 'db' : ''}`}
         width={newSize.width}
         height={newSize.height}
         viewBox="0 0 64 64"
         fill={color}
         xmlns="http://www.w3.org/2000/svg">
         <path
+          d="M52,10H12V6a2,2,0,0,1,2-2H50a2,2,0,0,1,2,2Z"
           fill={color}
-          d="M18,62v-4l5.25626-0.64264c1.16066-0.08098,1.32598-0.41163,1.49131-1.65664l6.62993-47.40145 c0.16533-1.24501,0.08435-1.57566-1.07631-1.66001L26,6V2h20v4l-5.58691,0.72361c-1.24501,0.16533-1.32936,0.33065-1.49469,1.57566 l-6.7109,47.32048c-0.16533,1.32598-0.16533,1.49131,1.07631,1.65664L38,58v4H18z"
+        />
+        <path
+          d="M59,12H5a2,2,0,0,0-2,2V58a2,2,0,0,0,2,2H59a2,2,0,0,0,2-2V14A2,2,0,0,0,59,12ZM21,20a4,4,0,1,1-4,4A4,4,0,0,1,21,20ZM52.87,49.493A1,1,0,0,1,52,50H12a1,1,0,0,1-.832-1.555l8-12a1,1,0,0,1,1.539-.152l7.185,7.185L39.2,28.4a.973.973,0,0,1,.852-.4,1,1,0,0,1,.8.484l12,20A1,1,0,0,1,52.87,49.493Z"
+          fill={color}
         />
       </svg>
     )
   }
 }
 
-Italic.defaultProps = {
+Image.defaultProps = {
   color: 'currentColor',
   size: 20,
   block: false,
 }
 
-Italic.propTypes = {
+Image.propTypes = {
   color: PropTypes.string,
   size: PropTypes.number,
   block: PropTypes.bool,
 }
 
-export default Italic
+export default Image
--- react/components/icon/Underline/index.js
+++ react/components/icon/Underline/index.js
@@ -7,38 +7,42 @@ const iconBase = {
   height: 12,
 }
 
-class Italic extends PureComponent {
+class Underline extends PureComponent {
   render() {
     const { color, size, block } = this.props
     const newSize = calcIconSize(iconBase, size)
 
     return (
       <svg
-        className={`${baseClassname('italic')} ${block ? 'db' : ''}`}
+        className={`${baseClassname('underline')} ${block ? 'db' : ''}`}
         width={newSize.width}
         height={newSize.height}
         viewBox="0 0 64 64"
         fill={color}
         xmlns="http://www.w3.org/2000/svg">
+        <path 
+          d="M55,4H37V8h4a2,2,0,0,1,2,2V34.51c0,5.742-4.187,10.879-9.9,11.436A11.012,11.012,0,0,1,21,35V10a2,2,0,0,1,2-2h4V4H9V8h4a2,2,0,0,1,2,2V34.4c0,9.046,6.811,16.957,15.836,17.561A17.018,17.018,0,0,0,49,35V10a2,2,0,0,1,2-2h4Z"
+          fill={color}
+        />
         <path
           fill={color}
-          d="M18,62v-4l5.25626-0.64264c1.16066-0.08098,1.32598-0.41163,1.49131-1.65664l6.62993-47.40145 c0.16533-1.24501,0.08435-1.57566-1.07631-1.66001L26,6V2h20v4l-5.58691,0.72361c-1.24501,0.16533-1.32936,0.33065-1.49469,1.57566 l-6.7109,47.32048c-0.16533,1.32598-0.16533,1.49131,1.07631,1.65664L38,58v4H18z"
+          d="M62,60H2a1,1,0,0,1,0-2H62a1,1,0,0,1,0,2Z"
         />
       </svg>
     )
   }
 }
 
-Italic.defaultProps = {
+Underline.defaultProps = {
   color: 'currentColor',
   size: 20,
   block: false,
 }
 
-Italic.propTypes = {
+Underline.propTypes = {
   color: PropTypes.string,
   size: PropTypes.number,
   block: PropTypes.bool,
 }
 
-export default Italic
+export default Underline
