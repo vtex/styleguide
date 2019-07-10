5d5f4c00153a8c2e3fe9f7766eebe6442e448f5e
Rerisson Matos <rerisson.matos@splab.ufcg.edu.br>
2019-07-10 17:44:31 -0300

    Check for document existence before creating dom elements

--- react/components/FloatingActionBar/index.js
+++ react/components/FloatingActionBar/index.js
@@ -2,26 +2,38 @@ import React from 'react'
 import Button from '../Button'
 import { createPortal } from 'react-dom'
 
-const FloatingActionBar = ({ onSave, onCancel, cancelLabel, saveLabel, parentId }) => {
-  const barContainer = document.createElement('div')
-  const barAsDom = document.getElementById(parentId)
-  barAsDom && barAsDom.appendChild(barContainer)
 
-  return createPortal(
-    <div className="w-100 bg-base shadow-1 tr pv5 pr7 fixed bottom-0">
-      <span className="mr5">
-        <Button variation="secondary" onClick={onCancel}>
-          {cancelLabel}
-        </Button>
-      </span>
-      <span>
-        <Button variation="primary" onClick={onSave}>
-          {saveLabel}
-        </Button>
-      </span>
-    </div>,
-    barContainer
-  )
+
+const FloatingActionBar = ({
+  onSave,
+  onCancel,
+  cancelLabel,
+  saveLabel,
+  parentId,
+}) => {
+  if (document && document.createElement) {
+    const barContainer = document.createElement('div')
+    const barAsDom = document.getElementById(parentId)
+    barAsDom && barAsDom.appendChild(barContainer)
+
+    return barContainer
+      ? createPortal(
+          <div className="w-100 bg-base shadow-1 tr pv5 pr7 absolute fixed bottom-0">
+            <span className="mr5">
+              <Button variation="secondary" onClick={onCancel}>
+                {cancelLabel}
+              </Button>
+            </span>
+            <span>
+              <Button variation="primary" onClick={onSave}>
+                {saveLabel}
+              </Button>
+            </span>
+          </div>,
+          barContainer
+        )
+      : null
+  }
 }
 
 export default FloatingActionBar
