758c126d4076e00196475bfbe41b2f5c1a30f577
Rerisson Matos <rerisson.matos@splab.ufcg.edu.br>
2019-07-15 15:15:22 -0300

    Document props

--- react/components/FloatingActionBar/index.js
+++ react/components/FloatingActionBar/index.js
@@ -6,24 +6,28 @@ import './action-bar.global.css'
 
 const FloatingActionBar = ({ onSave, onCancel, cancelLabel, saveLabel }) => (
   <div className="styleguide__floating-action-bar shadow-active w-100 bg-base tr pv5 pr7-ns absolute fixed bottom-0 left-0 z-2">
-      <span className="mr5">
-        <Button variation="secondary" onClick={onCancel}>
-          {cancelLabel}
-        </Button>
-      </span>
-      <span>
-        <Button variation="primary" onClick={onSave}>
-          {saveLabel}
-        </Button>
-      </span>
+    <span className="mr5">
+      <Button variation="secondary" onClick={onCancel}>
+        {cancelLabel}
+      </Button>
+    </span>
+    <span>
+      <Button variation="primary" onClick={onSave}>
+        {saveLabel}
+      </Button>
+    </span>
   </div>
 )
 
 FloatingActionBar.propTypes = {
+  /** Invoked when save button is pressed */
   onSave: PropTypes.func.isRequired,
+  /** Invoked when cancel button is pressed */
   onCancel: PropTypes.func,
-  cancelLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** Label to save button*/
   saveLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** Label to cancel button*/
+  cancelLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 }
 
 export default FloatingActionBar
