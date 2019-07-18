c4360217bff34ec891943bba5b341b760d8c5a8e
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-18 15:26:01 -0300

    Fix prettier

--- react/components/icon/Underline/index.js
+++ react/components/icon/Underline/index.js
@@ -20,14 +20,11 @@ class Underline extends PureComponent {
         viewBox="0 0 64 64"
         fill={color}
         xmlns="http://www.w3.org/2000/svg">
-        <path 
+        <path
           d="M55,4H37V8h4a2,2,0,0,1,2,2V34.51c0,5.742-4.187,10.879-9.9,11.436A11.012,11.012,0,0,1,21,35V10a2,2,0,0,1,2-2h4V4H9V8h4a2,2,0,0,1,2,2V34.4c0,9.046,6.811,16.957,15.836,17.561A17.018,17.018,0,0,0,49,35V10a2,2,0,0,1,2-2h4Z"
           fill={color}
         />
-        <path
-          fill={color}
-          d="M62,60H2a1,1,0,0,1,0-2H62a1,1,0,0,1,0,2Z"
-        />
+        <path fill={color} d="M62,60H2a1,1,0,0,1,0-2H62a1,1,0,0,1,0,2Z" />
       </svg>
     )
   }
