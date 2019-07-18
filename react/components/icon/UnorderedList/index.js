e1e05616350ff24d450388bde421f7d1110b812f
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-18 15:20:33 -0300

    Fix prettier

--- react/components/icon/UnorderedList/index.js
+++ react/components/icon/UnorderedList/index.js
@@ -19,11 +19,10 @@ class UnorderedList extends PureComponent {
         height={newSize.height}
         viewBox="0 0 64 64"
         fill={color}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle fill={color} cx="8" cy="12" r="6"/>
-        <circle fill={color} cx="8" cy="32" r="6"/>
-        <circle fill={color} cx="8" cy="52" r="6"/>
+        xmlns="http://www.w3.org/2000/svg">
+        <circle fill={color} cx="8" cy="12" r="6" />
+        <circle fill={color} cx="8" cy="32" r="6" />
+        <circle fill={color} cx="8" cy="52" r="6" />
         <path d="M61,13H21a1,1,0,0,1,0-2H61a1,1,0,0,1,0,2Z" fill={color} />
         <path d="M46,33H21a1,1,0,0,1,0-2H46a1,1,0,0,1,0,2Z" fill={color} />
         <path d="M61,53H21a1,1,0,0,1,0-2H61a1,1,0,0,1,0,2Z" fill={color} />
