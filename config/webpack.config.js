4978bfb9e7d5595569be1964b98d3c80aef9b9b1
Estácio Pereira <estacio.pereira@gmail.com>
2019-07-25 23:13:58 -0300

    Remove some unnecessary dependencies and fix Layout documentation

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,135 +1,32 @@
-// @remove-on-eject-begin
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-// @remove-on-eject-end
 // eslint-disable-next-line import/unambiguous
 'use strict'
 
-var path = require('path')
-
+const path = require('path')
 const autoprefixer = require('autoprefixer')
 const webpack = require('webpack')
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
-const paths = require('./paths')
 
-// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
-// injected into the application via DefinePlugin in Webpack configuration.
-const REACT_APP = /^REACT_APP_/i
+const appSrc = path.join(__dirname, '..', 'react')
+const appNodeModules = path.join(__dirname, '..', 'react', 'node_modules')
 
-// ENV ***********************************************************
-
-function getClientEnvironment(publicUrl) {
-  const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
-    .reduce(
-      (env, key) => {
-        env[key] = process.env[key]
-        return env
-      },
-      {
-        // Useful for determining whether we’re running in production mode.
-        // Most importantly, it switches React into the correct mode.
-        NODE_ENV: process.env.NODE_ENV || 'development',
-        // Useful for resolving the correct path to static assets in `public`.
-        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
-        // This should only be used as an escape hatch. Normally you would put
-        // images into the `src` and `import` them in code to get their paths.
-        PUBLIC_URL: publicUrl,
-      }
-    )
-  // Stringify all values so we can feed into Webpack DefinePlugin
-  const stringified = {
-    'process.env': Object.keys(raw).reduce((env, key) => {
-      env[key] = JSON.stringify(raw[key])
-      return env
-    }, {}),
-  }
-
-  return { raw, stringified }
-}
-
-// *****************************************************************
-
-const appPackage = require(paths.appPackageJson)
-
-// Webpack uses `publicPath` to determine where the app is being served from.
-// In development, we always serve from the root. This makes config easier.
-const publicPath = '/'
-// `publicUrl` is just like `publicPath`, but we will provide it to our app
-// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
-// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = ''
-// Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl)
-
-const component = appPackage.component
-
-paths.appSrc = path.join(__dirname, '..', 'react')
-
+// Styleguidist handles a lot of webpack configurations, because it creates
+// the actual html
+// https://github.com/styleguidist/react-styleguidist/blob/981eeaaa83478c7b1077ece09900b9954ba1df69/src/scripts/make-webpack-config.js
 module.exports = {
-  entry: component
-    ? {
-        app: [
-          require.resolve('./polyfills'),
-          require.resolve('react-dev-utils/webpackHotDevClient'),
-          // paths.appIndexJs,
-        ],
-        component: paths.appComponentJs,
-      }
-    : [
-        // We ship a few polyfills by default:
-        require.resolve('./polyfills'),
-        // Include an alternative client for WebpackDevServer. A client's job is to
-        // connect to WebpackDevServer by a socket and get notified about changes.
-        // When you save a file, the client will either apply hot updates (in case
-        // of CSS changes), or refresh the page (in case of JS changes). When you
-        // make a syntax error, this client will display a syntax error overlay.
-        // Note: instead of the default WebpackDevServer client, we use a custom one
-        // to bring better experience for Create React App users. You can replace
-        // the line below with these two lines if you prefer the stock client:
-        // eslint-disable-next-line prefer-template
-        require.resolve('webpack-dev-server/client') + '?/',
-        require.resolve('webpack/hot/dev-server'),
-        // require.resolve('react-dev-utils/webpackHotDevClient'),
-        // Finally, this is your app's code:
-        paths.appIndexJs,
-        // We include the app code last so that if there is a runtime error during
-        // initialization, it doesn't blow up the WebpackDevServer client, and
-        // changing JS code would still trigger a refresh.
-      ],
-  output: {
-    // Next line is not used in dev but WebpackDevServer crashes without it:
-    path: path.join(paths.appBuild, appPackage.version),
-    // Add /* filename */ comments to generated require()s in the output.
-    pathinfo: true,
-    // This does not produce a real file. It's just the virtual path that is
-    // served by WebpackDevServer in development. This is the JS bundle
-    // containing code from all our entry points, and the Webpack runtime.
-    filename: 'static/js/[name].js',
-    // There are also additional JS chunk files if you use code splitting.
-    chunkFilename: 'static/js/[name].chunk.js',
-    // This is the URL  app is served from. We use "/" in development.
-    publicPath: publicPath,
-    // Point sourcemap entries to original disk location (format as URL on Windows)
-    devtoolModuleFilenameTemplate: info =>
-      path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
-  },
+  entry: [
+    require.resolve('./polyfills'),
+    // eslint-disable-next-line prefer-template
+    require.resolve('webpack-dev-server/client') + '?/',
+    require.resolve('webpack/hot/dev-server'),
+    // We don't need to set our code's entry. Styleguidist already does that for us.
+  ],
   module: {
     rules: [
       // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
       { parser: { requireEnsure: false } },
       {
-        test: /\.(ts|tsx)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(js|jsx)?$/,
+        test: /\.(js|jsx|ts|tsx)?$/,
         loader: 'babel-loader',
         options: {
           presets: [
@@ -152,7 +49,7 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.global\.css$/,
-        include: paths.appSrc,
+        include: appSrc,
         use: [
           require.resolve('style-loader'),
           {
@@ -185,7 +82,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: paths.appSrc,
+        exclude: appSrc,
         use: [
           require.resolve('style-loader'),
           {
@@ -218,7 +115,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        include: paths.appSrc,
+        include: appSrc,
         exclude: /\.global\.css$/,
         use: [
           require.resolve('style-loader'),
@@ -272,14 +169,10 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  // Lots of plugins are already handled for us by styleguidist
   plugins: [
     // Add module names to factory functions so they appear in browser profiler.
     new webpack.NamedModulesPlugin(),
-    // Makes some environment variables available to the JS code, for example:
-    // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
-    new webpack.DefinePlugin(env.stringified),
-    // This is necessary to emit hot updates (currently CSS only):
-    new webpack.HotModuleReplacementPlugin(),
     // Watcher doesn't work well if you mistype casing in a path so we use
     // a plugin that prints an error when you attempt to do this.
     // See https://github.com/facebookincubator/create-react-app/issues/240
@@ -288,7 +181,7 @@ module.exports = {
     // to restart the development server for Webpack to discover it. This plugin
     // makes the discovery automatic so you don't have to restart.
     // See https://github.com/facebookincubator/create-react-app/issues/186
-    new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+    new WatchMissingNodeModulesPlugin(appNodeModules),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
     // solution that requires the user to opt into importing specific locales.
