3fe9e55e67cccdc80574dc093e5d16186ea7e7e3
klynger <rafael.klynger@gmail.com>
2019-07-26 15:51:59 -0300

    Add some missing packages + Remove browserLIst from autoprefixer config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -55,12 +55,6 @@ module.exports = {
               plugins: () => [
                 require('postcss-flexbugs-fixes'),
                 autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
                   flexbox: 'no-2009',
                 }),
               ],
@@ -88,12 +82,6 @@ module.exports = {
               plugins: () => [
                 require('postcss-flexbugs-fixes'),
                 autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
                   flexbox: 'no-2009',
                 }),
               ],
@@ -123,12 +111,6 @@ module.exports = {
               plugins: () => [
                 require('postcss-flexbugs-fixes'),
                 autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
                   flexbox: 'no-2009',
                 }),
               ],
