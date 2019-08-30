a7d7aeb9db618d1fe979e7e1828dbf9891ee7e78
ohmyguigs <ohmyguigs@gmail.com>
2019-08-30 16:23:38 -0300

    Converts separator js to tsx and remove css horrors

---
+++ react/components/Conditions/Separator.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Separator({ label }) {
+  return (
+    <div>
+      <div
+        style={{ width: 'calc(100% + 34px)' }}
+        className="flex flex-row w-100 nowrap items-center mv3 nl5">
+        <hr className="ma0 b--black-10 bb bb-0 w-50" />
+        <span className="gray ph3 dib bg-white">{label}</span>
+        <hr className="ma0 b--black-10 bb bb-0 w-50" />
+      </div>
+    </div>
+  )
+}
