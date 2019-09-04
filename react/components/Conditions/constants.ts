a1f6d0653f50da128d800a4b78ca1bbe86f61e68
ohmyguigs <ohmyguigs@gmail.com>
2019-09-04 17:49:37 -0300

    Enhance Conditions typescript helper types and constants

---
+++ react/components/Conditions/constants.ts
@@ -0,0 +1,15 @@
+import { Labels } from './typings';
+
+export const DEFAULT_LABELS: Labels = {
+  addNewCondition: 'add new condition',
+  delete: 'Remove',
+  headerPrefix: 'Matching',
+  headerSufix: 'following conditions:',
+  operatorAll: 'all',
+  operatorAnd: 'and',
+  operatorAny: 'any',
+  operatorOr: 'or',
+}
+
+export const SMALL_ICON_SIZE = 14
+export const MEDIUM_ICON_SIZE = 16
