5c32ad4cba177927fce616b23a16aea344923e61
matheusps <cout.matheusps@gmail.com>
2019-09-26 14:21:43 -0300

    Fixed arrow directions

--- react/components/EXPERIMENTAL_TableTree/Tree/Arrow.tsx
+++ react/components/EXPERIMENTAL_TableTree/Tree/Arrow.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react'
 
-import CaretRight from '../../icon/CaretRight/index.js'
+import CaretUp from '../../icon/CaretUp/index.js'
 import CaretDown from '../../icon/CaretDown/index.js'
 import ButtonWithIcon from '../../ButtonWithIcon/index.js'
 
 const Arrow: FC<ArrowProps> = ({ active, onClick }) => {
-  const icon = active ? <CaretDown /> : <CaretRight />
+  const icon = active ? <CaretUp /> : <CaretDown />
   return (
     <span className="ph2">
       <ButtonWithIcon
