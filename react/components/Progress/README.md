9113521644a2e51e4029dfb9db210731e6270828
Emerson Laurentino <emersonlaurentino@hotmail.com>
2019-11-04 18:25:00 -0300

    refactor Progress

--- react/components/Progress/README.md
+++ react/components/Progress/README.md
@@ -8,7 +8,7 @@
     const decrement = () => setValue(value - 10)
     return (
       <div>
-        <Progress value={value} max={100} />
+        <Progress percent={value} />
         <div className="flex mt6">
           <Button onClick={increment} size="small">Increment</Button>
           <div className="ml3">
@@ -25,11 +25,11 @@
 ### Do not show information
 
 ```jsx
-  <Progress value={25} max={100} showInfo={false} />
+  <Progress percent={25} />
 ```
 
 ### Completed
 
 ```jsx
-  <Progress value={100} max={100} />
+  <Progress percent={100} />
 ```
--- react/components/Progress/index.tsx
+++ react/components/Progress/index.tsx
@@ -4,40 +4,25 @@ import React from 'react'
 import styles from './styles.css'
 
 const propTypes = {
-  value: PropTypes.number.isRequired,
-  max: PropTypes.number.isRequired,
-  showInfo: PropTypes.bool,
+  percent: PropTypes.number.isRequired,
 }
 
 const Progress: React.FC<PropTypes.InferProps<typeof propTypes>> = props => {
-  const percentage = `${Math.round((props.value / props.max) * 100)}%`
   const barClasses = cn(
     styles.progressHeight,
     'br4 overflow-hidden bg-light-silver'
   )
   const stepClasses = cn(styles.progressHeight, 'br4', {
-    'bg-action-primary': props.value !== props.max,
-    'bg-success': props.value === props.max,
+    'bg-action-primary': props.percent !== 100,
+    'bg-success': props.percent === 100,
   })
   return (
-    <div className="flex flex-column">
-      {props.showInfo && (
-        <div className="flex justify-between mb3 f6 near-black">
-          <div>{percentage}</div>
-          <div>{`${props.value} / ${props.max}`}</div>
-        </div>
-      )}
-      <div className={barClasses}>
-        <div className={stepClasses} style={{ width: percentage }} />
-      </div>
+    <div className={barClasses}>
+      <div className={stepClasses} style={{ width: `${props.percent}%` }} />
     </div>
   )
 }
 
 Progress.propTypes = propTypes
-
-Progress.defaultProps = {
-  showInfo: true,
-}
 
 export default Progress
