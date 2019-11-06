4dacb80c36d90ca3c21f4fc64304beb09ceb00c7
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-11-06 13:55:33 -0300

    Fix identation

--- react/components/Charts/LineChart/constants.ts
+++ react/components/Charts/LineChart/constants.ts
@@ -1,17 +1,20 @@
 import { LineProps } from 'recharts'
+
 export const colors = [
-    '#EE7850',
-    '#4CAABF',
-    '#9F50B6',
-    '#479A5E'
+  '#EE7850',
+  '#4CAABF',
+  '#9F50B6',
+  '#479A5E'
 ]
+
 type DefaultLineProps = Pick<LineProps, 'type' | 'strokeWidth' | 'dot'>
 export const defaultProps: DefaultLineProps = {
-    type: 'monotone',
-    strokeWidth: 3,
-    dot: false,
+  type: 'monotone',
+  strokeWidth: 3,
+  dot: false,
 }
+
 export const tooltipProps = {
-    cursor: false,
-    labelStyle: {color: '#828282'}
+  cursor: false,
+  labelStyle: {color: '#828282'}
 }
--- react/components/Charts/commonProps.ts
+++ react/components/Charts/commonProps.ts
@@ -1,15 +1,15 @@
 export const commonDefaultProps = {
-    container:{
-        height: 300,
-        width: '100%'
-    },
-    axis: {
-        axisLine: false,
-        tickLine: false,
-        padding: { left: 20 }
-    },
-    grid: {
-        horizontal: false,
-        vertical: false,
-    }
+  container:{
+    height: 300,
+    width: '100%'
+  },
+  axis: {
+    axisLine: false,
+    tickLine: false,
+    padding: { left: 20 }
+  },
+  grid: {
+    horizontal: false,
+    vertical: false,
+  }
 }
