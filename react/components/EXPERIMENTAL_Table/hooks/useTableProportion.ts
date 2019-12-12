ad9214fa1bb86a598136a0e8c0ba7d16e89f56b4
matheusps <cout.matheusps@gmail.com>
2019-12-12 13:13:31 -0300

    Fix PR issues

--- react/components/EXPERIMENTAL_Table/hooks/useTableProportion.ts
+++ react/components/EXPERIMENTAL_Table/hooks/useTableProportion.ts
@@ -19,11 +19,10 @@ export default function useTableProportion({ columns, ratio }: ProportionData) {
 
 function calculateWidths(columns: Array<Column>, ratio: Array<number>) {
   const slicedRatio = ratio.slice(0, columns.length)
-
   const sum = (acc: number, num: number) => acc + num
   const ratioSum = slicedRatio.reduce(sum, 0)
-  const minPortion = 100 / ratioSum
-
+  const fullPortion = 100
+  const minPortion = fullPortion / ratioSum
   return slicedRatio.map(value => `${value * minPortion}%`)
 }
 
