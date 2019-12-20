67f6a6394745dc4689654b718c528f5a9d741263
matheusps <cout.matheusps@gmail.com>
2019-12-20 11:33:47 -0300

    Separate payments into simple data

--- react/components/EXPERIMENTAL_Table/sampleData.ts
+++ react/components/EXPERIMENTAL_Table/sampleData.ts
@@ -70,3 +70,54 @@ export const products = [
     wholesalePrice: 950,
   },
 ]
+
+export const payments = [
+  {
+    icon: 'FaCcPaypal',
+    id: '1',
+    name: 'Paypal',
+    status: 'ACTIVE',
+  },
+  {
+    icon: 'FaCcMastercard',
+    id: '2',
+    name: 'Mastercard',
+    status: 'ACTIVE',
+  },
+  {
+    icon: 'FaCcDinersClub',
+    id: '3',
+    name: 'Dinners Club',
+    status: 'ACTIVE',
+  },
+  {
+    icon: 'FaCcDiscover',
+    id: '4',
+    name: 'Discover',
+    status: 'INACTIVE',
+  },
+  {
+    icon: 'FaCcAmazonPay',
+    id: '5',
+    name: 'Amazon Pay',
+    status: 'INACTIVE',
+  },
+  {
+    icon: 'FaCcApplePay',
+    id: '6',
+    name: 'Apple Pay',
+    status: 'ACTIVE',
+  },
+  {
+    icon: 'FaCcVisa',
+    id: '7',
+    name: 'Visa',
+    status: 'ACTIVE',
+  },
+  {
+    icon: 'FaCcAmex',
+    id: '8',
+    name: 'American Express',
+    status: 'INACTIVE',
+  },
+]
