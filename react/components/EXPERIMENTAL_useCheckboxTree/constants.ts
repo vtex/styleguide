3cbc74268dfa3680c3a5b4c938e9a9af52a9c4e1
matheusps <cout.matheusps@gmail.com>
2020-01-13 18:22:26 -0300

    Fix useCheckboxeTree lint issues

--- react/components/EXPERIMENTAL_useCheckboxTree/constants.ts
+++ react/components/EXPERIMENTAL_useCheckboxTree/constants.ts
@@ -1,5 +1,10 @@
-export const defaultComparatorCurry = (item: any) => (candidate: any) =>
-  item.id === candidate.id
+type itemWithID = {
+  id: string
+}
+
+export const defaultComparatorCurry = (item: itemWithID) => (
+  candidate: itemWithID
+) => item.id === candidate.id
 
 export const ROOT_KEY = 'id'
 export const ROOT_VALUE = 'VTEX_CheckboxTreeRoot'
