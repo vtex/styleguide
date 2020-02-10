7aaeb4bef81494faf4eec47b8f1fe67e8d099413
Mariana Mendes <mariana.silva@ccc.ufcg.edu.br>
2020-02-10 16:09:36 -0300

    Fix highlight color of a selected row

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Changed
+
+- Color of active row on `EXPERIMENTAL_Table`.
+
 ## [9.112.2] - 2020-02-27
 
 ### Fixed
--- react/components/EXPERIMENTAL_Table/DataTable/Row.tsx
+++ react/components/EXPERIMENTAL_Table/DataTable/Row.tsx
@@ -12,15 +12,23 @@ const Row: FC<RowProps> & RowComposites = ({
   motion,
   highlightOnHover,
 }) => {
+  const LIGHT_BLUE = '#DBE9FD'
+
   const className = classNames('w-100 truncate overflow-x-hidden', {
     'pointer hover-c-link hover-bg-muted-5': onClick,
     'hover-bg-muted-5': highlightOnHover,
     'c-on-base bg-action-secondary': active,
+
   })
   const style = {
     height,
     ...motion,
   }
+
+  if (active) {
+    style['background-color'] = LIGHT_BLUE
+  }
+
   return (
     <tr style={style} onClick={onClick} className={className}>
       {children}
