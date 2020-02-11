8b689de4a0aec799c2985a2fe3e00158658355d6
Gustavo Rosolem <guspace92@hotmail.com>
2020-02-11 12:15:11 -0300

    v0 of icons from styles builder

--- react/components/icon/Columns/index.js
+++ react/components/icon/Columns/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import { calcIconSize, baseClassname } from '../utils'
+import { calcIconSize, baseClassname, IconBase } from '../utils'
 
 const iconBaseDimensions = {
   width: 16,
@@ -14,13 +14,7 @@ class Columns extends PureComponent {
     const newSize = calcIconSize(iconBaseDimensions, size)
 
     return (
-      <svg
-        className={`${baseClassname('columns')} ${block ? 'db' : ''}`}
-        width={newSize.width}
-        height={newSize.height}
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
+      <IconBase size={newSize} block={block} id="icon-columns">
         <path d="M1 4H15V15H1V4Z" stroke={color} strokeWidth="2" />
         <path
           d="M0 1C0 0.447715 0.447715 0 1 0H15C15.5523 0 16 0.447715 16 1V5H0V1Z"
@@ -34,7 +28,7 @@ class Columns extends PureComponent {
           stroke={color}
           strokeWidth="2"
         />
-      </svg>
+      </IconBase>
     )
   }
 }
