88b0eb0f4391bd7c907719a39ffed175ffc1b817
igorbrasileiro <brasileiro456@gmail.com>
2020-03-10 22:31:52 -0300

    add OptionsDots and handleResizeWindow

--- react/components/Tabs/index.tsx
+++ react/components/Tabs/index.tsx
@@ -1,25 +1,83 @@
 import React, { cloneElement } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types'
 
-const Tabs = ({ children, fullWidth, sticky, testId }) => {
+import Tab from './Tab'
+import OptionsDots from '../icon/OptionsDots'
+
+const propTypes = {
+  children: PropTypes.node,
+  fullWidth: PropTypes.bool,
+  sticky: PropTypes.bool,
+}
+
+const Tabs: React.FC<InferProps<typeof propTypes>> = ({
+  children,
+  fullWidth,
+  sticky,
+}) => {
+  const tabsContainerRef = React.useRef(null)
+  const [showMoreTabsButton, setShowMoreTabsButton] = React.useState(false)
+
   const childrenArray = [].concat(children)
 
-  const selectedTab = childrenArray.find(child => child.props.active)
-
+  const selectedTab: Tab = childrenArray.find(
+    child => (child as Tab).props.active
+  )
   const content = selectedTab && selectedTab.props.children
+
+  const handleResizeWindow = React.useCallback(() => {
+    if (tabsContainerRef.current) {
+      const tabsContainerWidth = tabsContainerRef.current.clientWidth
+      let hideTabs = false
+
+      const childrens = tabsContainerRef.current.children
+      let sumTabWidths = 0
+      for (let childIndex = 0; childIndex < childrens.length; childIndex++) {
+        const child = childrens[childIndex]
+        sumTabWidths += child.clientWidth
+        if (!hideTabs && sumTabWidths > tabsContainerWidth) {
+          hideTabs = true
+          break
+        }
+      }
+
+      setShowMoreTabsButton(hideTabs)
+    }
+  }, [tabsContainerRef])
+
+  React.useEffect(() => {
+    const hasWindow = !(
+      typeof window === 'undefined' || typeof window.Element === 'undefined'
+    )
+    hasWindow && handleResizeWindow() // this line will have effect if tabs has minimum width, need it
+    hasWindow && window.addEventListener('resize', handleResizeWindow)
+
+    return () => {
+      hasWindow && window.removeEventListener('resize', handleResizeWindow)
+    }
+  }, [handleResizeWindow])
+
   return (
     <div
       data-testid={testId}
       className={`vtex-tabs w-100 h-100 flex flex-column ${
         sticky ? 'overflow-y-hidden' : ''
-      }`}
-    >
-      <div className="vtex-tabs__nav flex flex-row bb b--muted-4">
-        {childrenArray.map((child, index) =>
-          cloneElement(child, {
-            fullWidth,
-            key: child.props.key != null ? child.props.key : index,
-          })
+      }`}>
+      <div className="flex">
+        <div
+          className="vtex-tabs__nav inline-flex flex-row bb b--muted-4 w-100"
+          ref={tabsContainerRef}>
+          {childrenArray.map((child, index) =>
+            cloneElement(child, {
+              fullWidth,
+              key: child.props.key != null ? child.props.key : index,
+            })
+          )}
+        </div>
+        {showMoreTabsButton && (
+          <button className="vtex-tab__button bt-0 bl-0 br-0 bb-0">
+            <OptionsDots />
+          </button>
         )}
       </div>
       <div
@@ -38,12 +96,6 @@ Tabs.defaultProps = {
   sticky: false,
 }
 
-Tabs.propTypes = {
-  /** Data attribute */
-  testId: PropTypes.string,
-  children: PropTypes.arrayOf(PropTypes.node),
-  fullWidth: PropTypes.bool,
-  sticky: PropTypes.bool,
-}
+Tabs.propTypes = propTypes
 
 export default Tabs
