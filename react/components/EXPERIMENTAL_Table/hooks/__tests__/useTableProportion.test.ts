866a5a61eac0aa376bbd1982db3676ef9f462c03
matheusps <cout.matheusps@gmail.com>
2020-03-11 17:28:23 -0300

    added tests for useTableProportion

---
+++ react/components/EXPERIMENTAL_Table/hooks/__tests__/useTableProportion.test.ts
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import useTableProportion from '../useTableProportion'
+
+describe('Table V2 @ hooks/useTableMeasures spec', () => {
+  const columns = [{ id: 'name' }, { id: 'location' }, { id: 'company' }]
+
+  const ratio = [2, 1, 1]
+
+  it('calculates widths correctly', () => {
+    const { result } = renderHook(() => useTableProportion({ columns, ratio }))
+
+    expect(result.current.sizedColumns).toEqual([
+      { id: 'name', width: '50%' },
+      { id: 'location', width: '25%' },
+      { id: 'company', width: '25%' },
+    ])
+  })
+})
