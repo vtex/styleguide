696abd59363143ca59a9614bc87fc28d7dc1ef85
matheusps <cout.matheusps@gmail.com>
2020-03-11 17:50:53 -0300

    added useTableVisibility tests

---
+++ react/components/EXPERIMENTAL_Table/hooks/__tests__/useTableVisibility.test.ts
@@ -0,0 +1,77 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import useTableVisibility from '../useTableVisibility'
+
+describe('Table V2 @ hooks/useTableVisibility spec', () => {
+  const columns = [{ id: 'name' }, { id: 'location' }, { id: 'company' }]
+  const ids = columns.map(col => col.id)
+
+  it('toggles correctly', () => {
+    const { result } = renderHook(() =>
+      useTableVisibility({ columns, hiddenColumns: ['name'] })
+    )
+
+    expect(result.current.hiddenColumns).toEqual(['name'])
+    expect(result.current.visibleColumns).toEqual([
+      { id: 'location' },
+      { id: 'company' },
+    ])
+
+    act(() => {
+      result.current.toggleColumn('name')
+    })
+
+    expect(result.current.hiddenColumns).toEqual([])
+    expect(result.current.visibleColumns).toEqual(columns)
+
+    act(() => {
+      result.current.toggleColumn('location')
+    })
+
+    expect(result.current.hiddenColumns).toEqual(['location'])
+    expect(result.current.visibleColumns).toEqual([
+      { id: 'name' },
+      { id: 'company' },
+    ])
+
+    act(() => {
+      result.current.toggleColumn('company')
+    })
+
+    expect(result.current.hiddenColumns).toEqual(['location', 'company'])
+    expect(result.current.visibleColumns).toEqual([{ id: 'name' }])
+  })
+
+  it('hiddes all columns', () => {
+    const { result } = renderHook(() => useTableVisibility({ columns }))
+
+    expect(result.current.hiddenColumns).toEqual([])
+    expect(result.current.visibleColumns).toEqual(columns)
+
+    act(() => {
+      result.current.hideAllColumns()
+    })
+
+    expect(result.current.hiddenColumns).toEqual(ids)
+    expect(result.current.visibleColumns).toEqual([])
+  })
+
+  it('shows all columns', () => {
+    const { result } = renderHook(() =>
+      useTableVisibility({
+        columns,
+        hiddenColumns: ['name', 'location', 'company'],
+      })
+    )
+
+    expect(result.current.hiddenColumns).toEqual(ids)
+    expect(result.current.visibleColumns).toEqual([])
+
+    act(() => {
+      result.current.showAllColumns()
+    })
+
+    expect(result.current.hiddenColumns).toEqual([])
+    expect(result.current.visibleColumns).toEqual(columns)
+  })
+})
