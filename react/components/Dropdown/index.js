c1c5a6939bc0d2c7747695177ff59e8c988bc751
Augusto Lazaro <aemlazaro@gmail.com>
2020-03-17 16:44:04 -0300

    Dont use variable before declare it

--- react/components/Dropdown/index.js
+++ react/components/Dropdown/index.js
@@ -104,6 +104,9 @@ class Dropdown extends Component {
       selectTestId,
     } = this.props
 
+    const valueLabel = this.getValueLabel()
+    const showCaption = !valueLabel
+
     const hasValidInitialValue =
       this.getOptionFromValue(this.initialValue) !== null
 
