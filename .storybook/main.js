dc2101d227b188de992aca658206b5c87daeddb2
Thayanne Sousa <thayannevls@gmail.com>
2020-04-07 18:45:10 -0300

    change storybook to use our custom webpack config and add vtex-tachyons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
-module.exports = {
-  stories: ['../react/components/**/*.stories.(ts|tsx)'],
-  webpackFinal: async config => {
-    config.module.rules = [
-      ...config.module.rules,
-      {
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader')
-      }
-    ]
+const path = require('path');
 
-    config.resolve = {
-      ...config.resolve,
-      extensions: ['.ts', '.tsx'],
-    }
-    
-    return config
-  },
+const custom = require('../config/webpack.config.js')
+
+module.exports = {
+  stories: ['../react/components/**/*.stories.tsx'],
+  webpackFinal: (config) => {
+    config.resolve.extensions.push('.ts', '.tsx')
+    return { ...config, module: { ...config.module, rules: custom.module.rules } }
+  }
 }
