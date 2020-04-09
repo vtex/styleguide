7b465bdcb2b60931ee37b7894270cc5ed5c6b463
Thayanne Sousa <thayannevls@gmail.com>
2020-04-08 21:26:36 -0300

    Add playground in PULL_REQUEST_TEMPLATE and fix lint problems in playground

--- react/playground/All.tsx
+++ react/playground/All.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 /*
   This story should contain all our components.
 */
-const All = () => (<h1>Under construction</h1>)
+const All = () => <h1>Under construction</h1>
 
 export default All
--- react/playground/ExamplePage.tsx
+++ react/playground/ExamplePage.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 /*
   This story should have an example page using our components.
 */
-const ExamplePage = () => (<h1>Under construction</h1>)
+const ExamplePage = () => <h1>Under construction</h1>
 
 export default ExamplePage
