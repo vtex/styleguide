143e95bf6b71137ae2b2f70d2b36e77d62f21077
Thayanne Sousa <thayannevls@gmail.com>
2020-04-20 14:30:14 -0300

    Rewrite Modal to typescript and add composable components

--- react/components/Modal/BottomBar.tsx
+++ react/components/Modal/BottomBar.tsx
@@ -1,19 +1,29 @@
 import React, { forwardRef } from 'react'
+import classNames from 'classnames'
 
-const BottomBar = forwardRef<HTMLDivElement, { children: React.ReactNode }>(
-  function BottomBar({ children }, forwardedRef) {
-    return (
-      <div
-        className={`
-        flex justify-content flex-row-reverse 'ph6 ph8-ns pv5 pv6-ns '
-        }
-      `}
-        ref={forwardedRef}
-      >
-        {children}
-      </div>
-    )
-  }
-)
+export interface Props {
+  showBorder?: boolean
+  responsiveFullScreen?: boolean
+  children?: React.ReactNode
+}
+
+const BottomBar = forwardRef<HTMLDivElement, Props>(function BottomBar(
+  { showBorder, responsiveFullScreen, children },
+  forwardedRef
+) {
+  if (!children) return <></>
+  return (
+    <div
+      className={classNames('flex justify-content flex-row-reverse', {
+        'bt b--muted-4': showBorder,
+        'ph7 pv5 ph8-ns pv6-ns': responsiveFullScreen,
+        'ph6 ph8-ns pv5 pv6-ns': !responsiveFullScreen,
+      })}
+      ref={forwardedRef}
+    >
+      {children}
+    </div>
+  )
+})
 
 export default BottomBar
