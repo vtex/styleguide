fc7cb69e88a46dab28b020cd1a4b2aafd84b6fbf
Thayanne Sousa <thayannevls@gmail.com>
2020-04-22 18:51:33 -0300

    Add size props in Modal

--- react/components/Modal/BottomBar.tsx
+++ react/components/Modal/BottomBar.tsx
@@ -5,6 +5,7 @@ export interface Props {
   showBorder?: boolean
   responsiveFullScreen?: boolean
   children?: React.ReactNode
+  size: 'small' | 'medium' | 'large'
 }
 
 const BottomBar = forwardRef<HTMLDivElement, Props>(function BottomBar(
@@ -14,11 +15,14 @@ const BottomBar = forwardRef<HTMLDivElement, Props>(function BottomBar(
   if (!children) return <></>
   return (
     <div
-      className={classNames('flex justify-content flex-row-reverse', {
-        'bt b--muted-4': showBorder,
-        'ph7 pv5 ph8-ns pv6-ns': responsiveFullScreen,
-        'ph6 ph8-ns pv5 pv6-ns': !responsiveFullScreen,
-      })}
+      className={classNames(
+        'flex justify-content flex-row-reverse min-h-regular-ns min-h-small pv6-ns ph8-ns pv7 ph6',
+        {
+          'bt b--muted-4': showBorder,
+          pb7: responsiveFullScreen,
+        }
+      )}
+      style={responsiveFullScreen ? { marginTop: 'auto' } : {}}
       ref={forwardedRef}
     >
       {children}
