c8ab2c4921a059adf0e53c6f0452e52e5d8a2812
matheusps <cout.matheusps@gmail.com>
2020-05-06 18:26:17 -0300

    Remove useTableContext

--- react/EXPERIMENTAL_useTableContext.ts
+++
@@ -1,7 +0,0 @@
-import { useMeasuresContext as measures } from './components/EXPERIMENTAL_Table/context/measures'
-import { useBodyContext as body } from './components/EXPERIMENTAL_Table/context/body'
-import { useDataContext as data } from './components/EXPERIMENTAL_Table/context/data'
-import { useHeadContext as head } from './components/EXPERIMENTAL_Table/context/head'
-import { useLoadingContext as loading } from './components/EXPERIMENTAL_Table/context/loading'
-
-export default { measures, body, data, head, loading }
