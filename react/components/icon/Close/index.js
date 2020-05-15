5e28c0e272fd176dd0cd4527e0c1bd63c049070b
Felippe Nardi <felippe@nardi.me>
2020-05-15 15:11:09 -0300

    Make alert accessible

--- react/components/icon/Close/index.js
+++ react/components/icon/Close/index.js
@@ -11,11 +11,12 @@ const iconBase = {
 
 class Close extends PureComponent {
   render() {
-    const { color, size, block } = this.props
+    const { color, size, block, title } = this.props
     const newSize = calcIconSize(iconBase, size)
 
     return (
       <Svg name="close" size={newSize} block={block} viewBox="0 0 18 18">
+        {title && <title>{title}</title>}
         <g fill={color}>
           <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
         </g>
@@ -34,6 +35,7 @@ Close.propTypes = {
   color: PropTypes.string,
   size: PropTypes.number,
   block: PropTypes.bool,
+  title: PropTypes.string,
 }
 
 export default Close
