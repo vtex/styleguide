067fe319b77a0f5a20ec6392793c46b73ba2352f
Lucas Bebber <lucasbbebber@gmail.com>
2020-10-01 11:38:58 -0300

    WIP

--- react/components/Spinner/index.js
+++ react/components/Spinner/index.js
@@ -11,59 +11,47 @@ class Spinner extends React.Component {
     const { color, size, block } = this.props
 
     return (
-      <svg
-        className={`${baseClassname('spinner')} ${
-          !color ? 'c-action-primary' : ''
-        } ${block ? 'db' : ''}`}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid"
-        height={size}
-        width={size}>
+      <>
         <style>
           {`
             @keyframes vtex-spinner-rotate {
               from {
-                transform-origin: 50% 50%;
-                transform: rotate(0deg);
+                transform: translate3d(0,0,0) rotate(0deg);
               }
               to {
-                transform-origin: 50% 50%;
-                transform: rotate(360deg);
+                transform: translate3d(0,0,0) rotate(360deg);
               }
             }
 
-            @keyframes vtex-spinner-fill {
-              0% {
-                stroke-dasharray: 0 0 2 ${circ};
-              }
-              50% {
-                stroke-dasharray: 0 0 ${circ * 0.75} ${circ};
-              }
-              100% {
-                stroke-dasharray: 0 ${circ - 2} ${circ * 0.75} ${circ};
-              }
-            }
-
-            .vtex-spinner_circle {
-              animation: vtex-spinner-fill 1.25s infinite cubic-bezier(0.455, 0.030, 0.515, 0.955), vtex-spinner-rotate 0.625s infinite linear;
+            .${baseClassname('spinner')} {
+              transform-origin: 50% 50%;
+              animation: vtex-spinner-rotate 0.625s infinite linear;
             }
           `}
         </style>
-
-        <circle
-          className="vtex-spinner_circle"
-          cx="50"
-          cy="50"
-          fill="none"
-          r={radius}
-          stroke={color || 'currentColor'}
-          strokeWidth="10"
-          strokeDasharray={`0 0 ${circ * 0.75} ${circ}`}
-          strokeLinecap="round"
-          strokeDashoffset="1"
-        />
-      </svg>
+        <svg
+          className={`${baseClassname('spinner')} ${
+            !color ? 'c-action-primary' : ''
+          } ${block ? 'db' : 'dib'}`}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+          height={size}
+          width={size}>
+          <circle
+            className="vtex-spinner_circle"
+            cx="50"
+            cy="50"
+            fill="none"
+            r={radius}
+            stroke={color || 'currentColor'}
+            strokeWidth="10"
+            strokeDasharray={`0 0 ${circ * 0.75} ${circ}`}
+            strokeLinecap="round"
+            strokeDashoffset="1"
+          />
+        </svg>
+      </>
     )
   }
 }
