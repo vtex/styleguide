de952a1cf842112de904df3b6b12ddad8938e4a4
Ovi <covidiu97@gmail.com>
2021-03-15 14:29:30 +0200

    Scroll polyfill

---
+++ react/modules/scrollPollyfill.js
@@ -0,0 +1,7 @@
+function isFunction(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
+
+if (!isFunction(window.scroll)) {
+  window.scroll = window.scrollTo
+}
