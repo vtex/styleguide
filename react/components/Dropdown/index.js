6374e64be49eb45d271fd76998ab2b0450e00c72
Estacio Pereira <estacio.pereira@vtex.com.br>
2021-06-16 13:06:51 -0300

    Fix Dropdown component prettier

--- react/components/Dropdown/index.js
+++ react/components/Dropdown/index.js
@@ -244,7 +244,10 @@ class Dropdown extends Component {
                 </option>
               )}
               {options.map(option => (
-                <option disabled={option.disabled} key={option.value} value={option.value}>
+                <option
+                  disabled={option.disabled}
+                  key={option.value}
+                  value={option.value}>
                   {option.label}
                 </option>
               ))}
